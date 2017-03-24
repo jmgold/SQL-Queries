f0b0a63648a88a5db714303a7a5ccc1f666535a5
Jeremy Goldstein <jgoldstein@minlib.net>
2017-03-24 14:02:09 -0400

    commented out broken code

--- review file location count.sql
+++ review file location count.sql
@@ -1,14 +1,15 @@
 ﻿--Count the number of owning libraries for each title in a review file
 --Use to identify weeding candidates owned elsewhere in the network
 
+--item count and the sums do not produce accurate counts
 SELECT
 id2reckey(b.id)||'a' AS "bib_record",
 b.title AS "bib_title",
-count(bi.id) AS "item_count",
+--count(bi.id) AS "item_count",
 count(distinct(i.agency_code_num)) AS "location_count",
-string_agg(distinct(loc.location_code), ',') AS "location codes",
-SUM(i.checkout_total) AS "total checkouts",
-SUM(i.renewal_total) AS "total renewals"
+string_agg(distinct(loc.location_code), ',') AS "location codes"
+--SUM(i.checkout_total) AS "total checkouts",
+--SUM(i.renewal_total) AS "total renewals"
 FROM
 sierra_view.bib_view as b 
 JOIN sierra_view.bool_set as sb ON b.id = sb.record_metadata_id AND sb.bool_info_id = '46'
