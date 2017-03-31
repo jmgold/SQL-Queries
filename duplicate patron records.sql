b82b1bacf5e43e3c831af2f766abf0bd36bd38f9
Jeremy Goldstein <jgoldstein@minlib.net>
2017-03-31 15:07:13 -0400

    duplicate patron records

---
+++ duplicate patron records.sql
@@ -0,0 +1,32 @@
+﻿-- based on query by David Jones and shared over Sierra listserv 9/20/16
+-- Finds patron records sharing a name
+
+SELECT
+    id2reckey(p.id)||'a' AS pnumber,
+    n.first_name || ' ' || n.middle_name || ' ' || n.last_name AS Name,
+    a.addr1 AS Address,
+    -- || ' ' || a.city || ' ' || a.postal_code AS Address,
+    p.barcode AS Barcode
+FROM
+    sierra_view.patron_record_fullname as n
+JOIN
+    sierra_view.patron_view as p
+ON
+    n.patron_record_id=p.id
+JOIN
+    sierra_view.patron_record_address as a
+ON
+    a.patron_record_id=p.id
+WHERE
+    concat (n.first_name, n.middle_name, n.last_name) IN (
+         SELECT 
+              concat (n.first_name, n.middle_name, n.last_name)
+         FROM
+              sierra_view.patron_record_fullname AS n
+         GROUP BY
+              1
+         HAVING
+              count (n.id) > 1)
+ORDER BY
+2;
+              
