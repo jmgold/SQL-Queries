a3f7257400ae0f3fd1c3d5e7a28408456d3aa01e
Jeremy Goldstein <jgoldstein@minlib.net>
2017-04-21 14:08:17 -0400

    added volume field and cleaned up old data

---
+++ volume field problems.sql
@@ -0,0 +1,25 @@
+﻿--Identifies bib records with some but not all attached items use volume fields.
+
+select 'b'||bib.record_num||'a' as "record_num",
+bib.title as "title",
+count(case when (varfield.varfield_type_code = 'v' and varfield.field_content is not null) then 1 else null end) as "volume_count",
+count(item.id) as "item_count"
+from
+sierra_view.bib_view as bib
+join
+sierra_view.bib_record_item_record_link as link
+on
+bib.id = link.bib_record_id
+join
+sierra_view.item_record as item
+on
+item.id = link.item_record_id
+left join
+sierra_view.varfield as varfield
+on
+item.id = varfield.record_id and varfield.varfield_type_code = 'v'
+group by 1, 2
+having (count(item.id) - count(case when (varfield.varfield_type_code = 'v' and varfield.field_content is not null) then 1 else null end)) > 0
+AND count(case when (varfield.varfield_type_code = 'v' and varfield.field_content is not null) then 1 else null end) != 0
+
+
