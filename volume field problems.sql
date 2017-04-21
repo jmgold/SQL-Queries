4fdd8a983129b7a84ac689098d905576eefe9d30
Jeremy Goldstein <jgoldstein@minlib.net>
2017-04-21 14:38:17 -0400

    updated volume field report

--- volume field problems.sql
+++ volume field problems.sql
@@ -1,4 +1,5 @@
 ﻿--Identifies bib records with some but not all attached items use volume fields.
+--Use to identify records with incorrect item level holds
 
 select 'b'||bib.record_num||'a' as "record_num",
 bib.title as "title",
@@ -21,5 +22,7 @@ item.id = varfield.record_id and varfield.varfield_type_code = 'v'
 group by 1, 2
 having (count(item.id) - count(case when (varfield.varfield_type_code = 'v' and varfield.field_content is not null) then 1 else null end)) > 0
 AND count(case when (varfield.varfield_type_code = 'v' and varfield.field_content is not null) then 1 else null end) != 0
+-- 6 to 1 ratio of items to items with volumes seems like decent but imperfect marker of the point past which it is no longer an effective use of time to check each record
+AND (count(item.id)/ count(case when (varfield.varfield_type_code = 'v' and varfield.field_content is not null) then 1 else null end)) >= 6
 
 
