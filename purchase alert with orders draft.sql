8ba1e176169881aa489f4d327e47dd1f46f93c68
Jeremy Goldstein <jgoldstein@minlib.net>
2017-05-04 16:29:40 -0400

    failed attempt to make library specific alert

---
+++ purchase alert with orders draft.sql
@@ -0,0 +1,32 @@
+﻿SELECT
+'b'||b.record_id||'a' AS "Record_number",
+bp.best_title AS "Title",
+bp.best_author AS "Author",
+bp.publish_year AS "Publication_year",
+bp.material_code AS "MatType",
+COUNT(h.id) AS "Total_holds",
+COUNT(CASE WHEN h.pickup_location_code LIKE 'act%' then 1 ELSE NULL END) AS "Local_holds",
+COUNT(bi.id) AS "Total_items",
+COUNT(CASE WHEN i.location_code LIKE 'act%' THEN 1 ELSE NULL END) AS "Local_items",
+SUM(ocmf.copies) AS "Copies_on_order"
+FROM
+sierra_view.bib_record AS b
+JOIN sierra_view.bib_record_property AS bp
+ON b.id=bp.bib_record_id
+LEFT JOIN sierra_view.bib_record_item_record_link AS bi 
+ON bi.bib_record_id=b.id
+JOIN sierra_view.item_record AS i
+ON bi.item_record_id=i.id
+JOIN sierra_view.hold AS h
+ON (h.record_id=b.id OR h.record_id=bi.item_record_id)
+LEFT JOIN sierra_view.bib_record_order_record_link AS bo
+ON bo.bib_record_id=b.id
+LEFT JOIN sierra_view.order_record as o
+ON bo.order_record_id=o.id AND o.order_status_code='o' AND o.accounting_unit_code_num = '1'
+LEFT JOIN sierra_view.order_record_cmf AS ocmf
+ON o.id=ocmf.order_record_id
+WHERE
+h.status='0' AND h.is_frozen='FALSE'
+GROUP BY 1, 2, 3, 4, 5
+HAVING count(DISTINCT h.id)>0
+LIMIT 100;
--- purchase alert with orders.sql
+++ purchase alert with orders.sql
@@ -5,13 +5,15 @@ DROP TABLE IF EXISTS mvhdholds;
 
 /* This is query is run first and create the temp table populating with bibs that have holds over a particular threshold */
 CREATE TEMP TABLE mvhdholds AS
-SELECT b.id AS "bib_id", count(distinct h.id) as "hold_count",
+SELECT b.id AS "bib_id", 
+count(distinct h.id) as "hold_count",
 CASE
                 WHEN count(distinct i.id) IS NULL THEN 0
                 ELSE count(distinct i.id)
         END
         AS "item_count",
-count(distinct ia.id) as "avail_item_count", max(o1.order_count) AS "order_count",
+count(distinct ia.id) as "avail_item_count", 
+max(o1.order_count) AS "order_count",
 string_agg(distinct i.location_code, ' ' order by i.location_code desc) as "item_location",
 max(o1.order_location) AS "order_location",
 CASE
@@ -20,17 +22,25 @@ CASE
         END
         AS "order_copies"
 FROM sierra_view.bib_record b
-LEFT JOIN sierra_view.bib_record_item_record_link bri on bri.bib_record_id=b.id
-JOIN sierra_view.hold h ON (h.record_id=b.id OR h.record_id=bri.item_record_id)
-LEFT JOIN sierra_view.item_record i ON i.id=bri.item_record_id
-LEFT JOIN sierra_view.item_record ia ON ia.id=bri.item_record_id AND ia.item_status_code not in
+LEFT JOIN sierra_view.bib_record_item_record_link bri
+on bri.bib_record_id=b.id
+JOIN sierra_view.hold h
+ON (h.record_id=b.id OR h.record_id=bri.item_record_id)
+LEFT JOIN sierra_view.item_record i
+ON i.id=bri.item_record_id
+LEFT JOIN sierra_view.item_record ia
+ON ia.id=bri.item_record_id AND ia.item_status_code not in
 ('m','n','o','e','z','s','$','d','w')
 LEFT JOIN (
-        SELECT count(o.id) AS "order_count", sum(oc.copies) AS "order_copies",
-        string_agg(distinct oc.location_code, ' ' order by oc.location_code desc) AS "order_location", bro.bib_record_id AS "bib_id"
+        SELECT count(o.id) AS "order_count",
+        sum(oc.copies) AS "order_copies",
+        string_agg(distinct oc.location_code, ' ' order by oc.location_code desc) AS "order_location",
+        bro.bib_record_id AS "bib_id"
         FROM sierra_view.bib_record_order_record_link BRO
-        JOIN sierra_view.order_record o ON o.id=bro.order_record_id
-        JOIN sierra_view.order_record_cmf oc ON oc.order_record_id=o.id
+        JOIN sierra_view.order_record o
+        ON o.id=bro.order_record_id AND o.accounting_unit_code_num = '2'
+        JOIN sierra_view.order_record_cmf oc
+        ON oc.order_record_id=o.id
         WHERE o.order_status_code = 'o'
         GROUP BY bro.bib_record_id) o1 ON o1.bib_id=b.id
 WHERE
@@ -52,6 +62,7 @@ select 'b'||b.record_num||'a' AS "Record Number",
 brp.best_title AS "Title", 
 brp.best_author AS "Author", 
 brp.material_code AS "MatType",
+brp.publish_year AS "PublicationYear",
 max(mv.item_count) as "Total item count", 
 max(mv.avail_item_count) as "available item count",
 max(mv.order_copies) as "Total order copies",
@@ -62,8 +73,10 @@ max(mv.hold_count) as "hold count",
         END
         AS "Ratio"
 from sierra_view.bib_record_property brp
-JOIN mvhdholds mv ON mv.bib_id=brp.bib_record_id
-JOIN sierra_view.bib_view b ON b.id=brp.bib_record_id
+JOIN mvhdholds mv
+ON mv.bib_id=brp.bib_record_id
+JOIN sierra_view.bib_view b
+ON b.id=brp.bib_record_id
 GROUP BY b.record_num, brp.best_title, brp.best_author, brp.material_code
 HAVING
 (max(mv.item_count) + max(mv.order_copies))=0
