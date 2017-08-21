ad489301958455032169b343e890b3204d5003e8
Jeremy Goldstein <jgoldstein@minlib.net>
2017-08-21 13:34:10 -0400

    bad price field update

--- Bad price fields.sql
+++ Bad price fields.sql
@@ -1,11 +1,11 @@
 ﻿--Finds items where a barcode was scanned into the price field
 
 select
-'b'||id2reckey(record_id)||'a',
+id2reckey(record_id)||'a',
 location_code,
 price
 from
 sierra_view.item_record
 where
-price>'999999'
+price>'999999' or (price>='1000' and item_status_code != 'o' and itype_code_num < '221')
 order by 2
