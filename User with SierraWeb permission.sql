83862d2187b654f6b50e22b89dbfafb11c11e86b
Jeremy Goldstein <jgoldstein@minlib.net>
2017-09-01 10:14:19 -0400

    sierraweb permission

---
+++ User with SierraWeb permission.sql
@@ -0,0 +1,9 @@
+﻿select distinct(u.user_name)
+from
+sierra_view.iii_user_application_myuser u
+left join
+sierra_view.iii_user_desktop_option o
+on 
+u.iii_user_id=o.iii_user_id  and o.desktop_option_id='899'
+where o.id is null
+order by 1
