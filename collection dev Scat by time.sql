5de87e8b4b339f6a2d4fa5fdddca642f268a8f1f
Jeremy Goldstein <jgoldstein@minlib.net>
2017-09-06 15:14:44 -0400

    collection dev corrections

--- collection dev Scat by time.sql
+++ collection dev Scat by time.sql
@@ -15,16 +15,16 @@ COUNT (id) FILTER(WHERE last_checkout_gmt >= (localtimestamp - interval '3 years
 ROUND(CAST(COUNT(id) FILTER(WHERE last_checkout_gmt >= (localtimestamp - interval '3 years')) AS NUMERIC (12,2)) / CAST(count (id) AS NUMERIC (12,2)), 6) AS "Percentage_3_years",
 COUNT (id) FILTER(WHERE last_checkout_gmt >= (localtimestamp - interval '5 years')) AS "have_circed_within_5_years",
 ROUND(CAST(COUNT(id) FILTER(WHERE last_checkout_gmt >= (localtimestamp - interval '5 years')) AS NUMERIC (12,2)) / CAST(count (id) AS NUMERIC (12,2)), 6) AS "Percentage_5_years",
-COUNT (id) FILTER(WHERE last_checkout_gmt < (localtimestamp - interval '5 years')) AS "have_circed_within_5+_years",
-ROUND(CAST(COUNT(id) FILTER(WHERE last_checkout_gmt >= (localtimestamp - interval '5 years')) AS NUMERIC (12,2)) / CAST(count (id) AS NUMERIC (12,2)), 6) AS "Percentage_5+_years",
+COUNT (id) FILTER(WHERE last_checkout_gmt is not null) AS "have_circed_within_5+_years",
+ROUND(CAST(COUNT(id) FILTER(WHERE last_checkout_gmt is not null) AS NUMERIC (12,2)) / CAST(count (id) AS NUMERIC (12,2)), 6) AS "Percentage_5+_years",
 COUNT (id) FILTER(WHERE last_checkout_gmt is null) AS "0_circs",
 ROUND(CAST(COUNT(id) FILTER(WHERE last_checkout_gmt is null) AS NUMERIC (12,2)) / CAST(count (id) AS NUMERIC (12,2)), 6) AS "Percentage_0_circs",
 ROUND((COUNT(id) *(AVG(price) FILTER(WHERE price>'0' and price <'10000'))/(NULLIF((SUM(checkout_total) + SUM(renewal_total)),0))),2) AS "Cost_Per_Circ_By_AVG_price",
 round(cast(SUM(checkout_total) + SUM(renewal_total) as numeric (12,2))/cast(count (id) as numeric (12,2)), 2) as turnover,
-round(cast(count(id) as numeric (12,2)) / (select cast(count (id) as numeric (12,2))from sierra_view.item_record where location_code LIKE 'ntn%'), 6) as relative_item_total,
-round(cast(SUM(checkout_total) + SUM(renewal_total) as numeric (12,2)) / (SELECT cast(SUM(checkout_total) + SUM(renewal_total) as numeric (12,2)) from sierra_view.item_record where location_code LIKE 'ntn%'), 6) as relative_circ
+round(cast(count(id) as numeric (12,2)) / (select cast(count (id) as numeric (12,2))from sierra_view.item_record where location_code LIKE 'brk%'), 6) as relative_item_total,
+round(cast(SUM(checkout_total) + SUM(renewal_total) as numeric (12,2)) / (SELECT cast(SUM(checkout_total) + SUM(renewal_total) as numeric (12,2)) from sierra_view.item_record where location_code LIKE 'brk%'), 6) as relative_circ
 FROM
 sierra_view.item_record
-WHERE location_code LIKE 'ntn%'
+WHERE location_code LIKE 'brk%'
 GROUP BY 1
 ORDER BY 1;
