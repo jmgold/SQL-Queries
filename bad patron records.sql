025bc0cfa31d7416f94cd3b7a0ad899c11b3d5ec
Jeremy Goldstein <jgoldstein@minlib.net>
2018-02-01 08:49:06 -0500

    add patron queries
    bad patron records and patrons with duplicate charges

--- bad patron records.sql
+++ bad patron records.sql
@@ -20,12 +20,15 @@ p.id = v.record_id and varfield_type_code = 'z'
 where
 p.ptype_code = '1'
 and
-(p.home_library_code !~ 'actz'
+((p.home_library_code !~ '%z' and p.home_library_code is not null)
 or p.pcode3 != '1'
+--or patron agency is online
 or a.addr1 is null
 or a.city is null
 or a.postal_code !~ '\d{5}([ \-]\d{4})'
 or p.barcode is null
+--or p.barcode not valid
+--or duplicate barcode
 or (v.field_content is not null and v.field_content !~ '(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)'))
 order by
 2,1
---
+++ patrons with duplicate charges.sql
@@ -0,0 +1,11 @@
+﻿--Original query created by Sarah Frieldsmith and shared on Sierra list 1/31/18
+SELECT
+id2reckey(f.patron_record_id)
+FROM
+sierra_view.fine f
+WHERE
+f.charge_code = '6'
+GROUP BY
+f.patron_record_id, date(f.assessed_gmt), f.item_record_metadata_id, f.item_charge_amt
+HAVING
+count(*) > 1
