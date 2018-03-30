3f6ee0a8fe2bf19c421375945c10a483a13eb6a8
Jeremy Goldstein <jgoldstein@minlib.net>
2018-03-30 10:49:52 -0400

    unique patrons

---
+++ unique patron checkouts.sql
@@ -0,0 +1,9 @@
+﻿SELECT
+count(distinct(patron_record_id))
+FROM
+sierra_view.circ_trans
+where
+op_code = 'o'
+and
+transaction_gmt between '2018-03-28' and '2018-03-30'
+and stat_group_code_num in ('330','331')
