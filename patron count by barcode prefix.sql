ce88c5bdd7bc13191f469af3fec321f6a2c4094c
Jeremy Goldstein <jgoldstein@minlib.net>
2018-07-09 08:57:13 -0400

    annual reports

--- patron count by barcode prefix.sql
+++ patron count by barcode prefix.sql
@@ -46,8 +46,13 @@ WHEN p.barcode LIKE '21906%' THEN 'Woburn'
 ELSE 'Unknown'
 END AS PType,
 COUNT(p.id) AS Total_Patrons,
-COUNT(p.id) FILTER(WHERE p.activity_gmt > (localtimestamp - INTERVAL '1 year')) AS Active_Patrons
+COUNT(p.id) FILTER(WHERE p.activity_gmt > (localtimestamp - INTERVAL '1 year')) AS Active_Patrons,
+COUNT(p.id) FILTER(WHERE m.creation_date_gmt > (localtimestamp - INTERVAL '1 year')) AS New_Patrons
 FROM
 sierra_view.patron_view p
+JOIN
+sierra_view.record_metadata m
+ON
+p.record_num = m.record_num and m.record_type_code = 'p'
 GROUP BY 1
 ORDER BY 1
