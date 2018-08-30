3753235cff47cc9afe54bca76443fbabade5a2d7
Jeremy Goldstein <jgoldstein@minlib.net>
2018-08-30 16:24:00 -0400

    value calculator

---
+++ avg patron.sql
@@ -0,0 +1,8 @@
+﻿SELECT
+AVG(checkout_total) FILTER (where checkout_total > '0') as avg_checkout_total,
+MODE() WITHIN GROUP(order by date_part('year',birth_date_gmt)) FILTER (where checkout_total > '0') as avg_birth_year,
+MODE() WITHIN GROUP(order by(activity_gmt::date)) FILTER (where checkout_total > '0') as avg_active
+FROM
+sierra_view.patron_record
+WHERE
+activity_gmt > (localtimestamp - interval '2 years')
