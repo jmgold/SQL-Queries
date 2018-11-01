7e8bf83d9d0be82785e88469f3cb31f240363b80
Jeremy Goldstein <JGoldstein@MLN.LOCAL>
2018-11-01 13:13:48 -0400

    favorites for the year

---
+++ Seasonal booklists/Favorites for the year.sql
@@ -0,0 +1,29 @@
+﻿--Jeremy Goldstein
+--Minuteman Library Network
+
+--top 50 most requested titles in the network
+
+SELECT
+'http://find.minlib.net/iii/encore/record/C__R'||id2reckey(b.bib_record_id)   AS "field_booklist_entry_encore_url",
+best_title as title,
+best_author as field_booklist_entry_author,
+SUM(i.checkout_total),
+'https://syndetics.com/index.aspx?isbn='||SUBSTRING(MAX(s.content) FROM '[0-9]+')||'/SC.gif&client=minuteman' AS field_booklist_entry_cover
+FROM
+sierra_view.bib_record_property b
+JOIN sierra_view.subfield s
+ON
+b.bib_record_id = s.record_id AND s.marc_tag = '020' AND s.tag = 'a'
+JOIN
+sierra_view.bib_record_item_record_link bi
+ON
+b.bib_record_id = bi.bib_record_id
+JOIN
+sierra_view.item_record i
+ON
+bi.item_record_id = i.id AND SUBSTRING(i.location_code,4,1) NOT IN ('j')
+WHERE
+b.publish_year = '2018' AND b.material_code = 'a'
+GROUP BY 1,2,3
+ORDER BY 4 desc
+LIMIT 100
