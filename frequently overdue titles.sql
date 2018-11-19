2f43c607eafa9a2300e9e221adc7197dcd692fa2
Jeremy Goldstein <JGoldstein@MLN.LOCAL>
2018-11-19 15:44:51 -0500

    updates

--- frequently overdue titles.sql
+++ frequently overdue titles.sql
@@ -1,32 +1,39 @@
-﻿SELECT
+﻿--Jeremy Goldstein
+--Minuteman Library Network
+--
+--Produces list of the 50 titles that most frequently incur overdue fines
+
+SELECT
+--Link to Encore
 'https://find.minlib.net/iii/encore/record/C__R'||id2reckey(b.bib_record_id) AS field_booklist_entry_encore_url,
 b.best_title as title,
 b.best_author as field_booklist_entry_author,
+--Link to cover from Syndetics
 'https://syndetics.com/index.aspx?isbn='||SUBSTRING(MAX(s.content) FROM '[0-9]+')||'/SC.gif&client=minuteman' AS field_booklist_entry_cover
 FROM
-sierra_view.fine f
-JOIN
-sierra_view.record_metadata m
-ON
-f.item_record_metadata_id = m.id AND m.record_type_code = 'i'
-JOIN
-sierra_view.item_view i
-ON
-m.record_num = i.record_num
-JOIN
-sierra_view.bib_record_item_record_link l
-ON
-i.id = l.item_record_id
-AND SUBSTRING(i.location_code,4,1) NOT IN ('j', 'y')
-JOIN
 sierra_view.bib_record_property b
-ON
-l.bib_record_id = b.bib_record_id AND b.material_code = 'a'
+--Pull ISBN for cover image
 JOIN sierra_view.subfield s
 ON
 b.bib_record_id = s.record_id AND s.marc_tag = '020' AND s.tag = 'a'
+JOIN
+sierra_view.bib_record_item_record_link l
+ON
+b.bib_record_id = l.bib_record_id
+JOIN
+sierra_view.item_record i
+ON
+l.item_record_id = i.id
+--Limit to titles not in children's or YA
+AND SUBSTRING(i.location_code,4,1) NOT IN ('j','y')
+JOIN
+sierra_view.fine f
+ON
+--Limit to overdue charge_code
+i.id = f.item_record_metadata_id AND f.charge_code = '2'
 WHERE
-f.charge_code = '2'
+--limit to book
+b.material_code = 'a'
 GROUP BY 1,2,3
-ORDER BY COUNT(m.id) desc
+ORDER BY COUNT(i.id) desc
 LIMIT 50
--- top request at a library v2.sql
+++ top request at a library v2.sql
@@ -1,15 +1,19 @@
-﻿--Gathers the most requested titles at each location, for www.minlib.net/booklists/recommended-reads/top-request-by-library
---Jeremy Goldstein with enormous assist from Ray Voelker
+﻿--Jeremy Goldstein with Enormous assistance from Ray Voelker
+--Minuteman Library Network
+
+--Gathers the most requested title at each location in the network, for booklist located at https://www.minlib.net/booklists/recommended-reads/top-request-by-library
+
 
 DROP TABLE IF EXISTS temp_holds_data
 ;
-
+--gathers hold data used by temp_location_holds_counts table
 CREATE TEMP TABLE temp_holds_data AS
 SELECT
 h.pickup_location_code,
 h.record_id,
 r.record_type_code, 
 r.record_num,
+--reconciles bib,item and volume level holds
 CASE
     WHEN r.record_type_code = 'i' THEN (
         SELECT
@@ -44,7 +48,7 @@ ON
   r.id = h.record_id
 ;
 
-
+--gathers hold count for each title at a pickup location
 DROP TABLE IF EXISTS temp_location_holds_counts;
 CREATE TEMP TABLE temp_location_holds_counts AS
 SELECT
@@ -54,6 +58,10 @@ count(t.bib_record_id) as count_holds_on_title
 
 FROM
 temp_holds_data as t
+
+--Filter out out-of-network ILL's
+WHERE
+t.pickup_location_code != 'mlsz'
 
 GROUP BY
 t.pickup_location_code,
@@ -67,13 +75,15 @@ t.pickup_location_code,
 count_holds_on_title
 ;
 
-
+--Produces final list
 SELECT
 DISTINCT ON (field_booklist_entry_location)
 l.name AS field_booklist_entry_location,
+--Encore Link
 'https://find.minlib.net/iii/encore/record/C__R'||id2reckey(t.bib_record_id) AS field_booklist_entry_encore_url,
 b.best_title as title,
 b.best_author AS field_booklist_entry_author,
+--Cover image from Syndetics
 'https://syndetics.com/index.aspx?isbn='||SUBSTRING(MAX(s.content) FROM '[0-9]+')||'/SC.gif&client=minuteman' AS field_booklist_entry_cover,
 t.count_holds_on_title
 
@@ -89,6 +99,7 @@ FROM (
     t2.pickup_location_code
 ) AS c
 
+--Pull location name
 JOIN
 sierra_view.location_myuser l
 ON
@@ -104,7 +115,7 @@ JOIN
 sierra_view.bib_record_property b
 ON
 t.bib_record_id = b.bib_record_id
-
+--Grab ISBN for cover image
 JOIN sierra_view.subfield s
 ON
 b.bib_record_id = s.record_id AND s.marc_tag = '020' AND s.tag = 'a'
