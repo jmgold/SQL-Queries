db2220d4084f6195116e83c7ab61dce82b735768
Jeremy Goldstein <JGoldstein@MLN.LOCAL>
2018-11-21 12:01:51 -0500

    january lists

---
+++ Seasonal booklists/paris peace conference.sql
@@ -0,0 +1,49 @@
+﻿/*
+Jeremy Goldstein
+Minuteman Library Newtork
+
+Used to generate booklist at www.minlib.net
+*/
+SELECT *
+FROM(
+SELECT
+--link to Encore
+DISTINCT 'https://find.minlib.net/iii/encore/record/C__R'||id2reckey(b.bib_record_id)   AS field_booklist_entry_encore_url,
+B.best_title as title,
+B.best_author as field_booklist_entry_author,
+--Generate cover image from Syndetics
+'https://syndetics.com/index.aspx?isbn='||SUBSTRING(MAX(s.content) FROM '[0-9]+')||'/SC.gif&client=minuteman' AS field_booklist_entry_cover
+FROM
+sierra_view.bib_record_property b
+JOIN
+sierra_view.bib_record_item_record_link l
+ON
+b.bib_record_id = l.bib_record_id
+JOIN
+sierra_view.item_record i
+ON
+l.item_record_id = i.id
+--Limit to English titles
+JOIN
+sierra_view.bib_record br
+ON
+b.bib_record_id = br.id AND br.language_code = 'eng'
+--AND i.is_available_at_library = 'TRUE'
+AND i.item_status_code NOT IN ('m', 'n', 'z', 't', 'o', '$', '!', 'w', 'd', 'p', 'r', 'e', 'j', 'u', 'q', 'x', 'y', 'v')
+--Limit to adult collections
+AND SUBSTRING(i.location_code,4,1) NOT IN ('j','y')
+JOIN
+sierra_view.varfield_view v
+ON
+b.bib_record_id = v.record_id AND v.varfield_type_code = 'd' 
+--Limit to a subject
+AND ((v.field_content LIKE '%Paris Peace Conference|d(1919-1920)%' OR v.field_content LIKE '%World War, 1914-1918|xPeace%' OR v.field_content LIKE '%Treaty of Versailles|d(1919 June 28)%')AND v.field_content NOT LIKE '%Fiction%')
+--Grab ISBN for cover image
+JOIN sierra_view.subfield s
+ON
+b.bib_record_id = s.record_id AND s.marc_tag = '020' AND s.tag = 'a'
+WHERE
+b.material_code = 'a' AND b.publish_year >= '1970'
+GROUP BY 1,2,3) a
+ORDER BY RANDOM()
+LIMIT 25;
