044c83894ddb6e630533440166701c2c02e3cf10
Jeremy Goldstein <JGoldstein@MLN.LOCAL>
2018-11-26 15:10:49 -0500

    booklist updates

---
+++ Seasonal booklists/happy holidays.sql
@@ -0,0 +1,111 @@
+﻿(SELECT *
+FROM(
+SELECT
+--link to Encore
+DISTINCT 'https://find.minlib.net/iii/encore/record/C__R'||id2reckey(b.bib_record_id)   AS field_booklist_entry_encore_url,
+B.best_title as title,
+B.best_author as field_booklist_entry_author,
+'https://syndetics.com/index.aspx?isbn='||SUBSTRING(MAX(s.content) FROM '[0-9]+')||'/SC.gif&client=minuteman' AS field_booklist_entry_cover
+FROM
+sierra_view.bib_record_property b
+JOIN
+sierra_view.bib_record_item_record_link l
+ON
+b.bib_record_id = l.bib_record_id
+JOIN
+sierra_view.item_record i
+ON
+l.item_record_id = i.id
+AND i.is_available_at_library = 'TRUE'
+AND i.item_status_code NOT IN ('m', 'n', 'z', 't', 'o', '$', '!', 'w', 'd', 'p', 'r', 'e', 'j', 'u', 'q', 'x', 'y', 'v')
+AND i.itype_code_num NOT IN ('100', '101', '104', '105', '108', '150', '151', '152', '155', '156', '159', '160')
+AND SUBSTRING(i.location_code,4,1) NOT IN ('j','y')
+JOIN
+sierra_view.varfield_view v
+ON
+b.bib_record_id = v.record_id AND v.varfield_type_code = 'd' 
+--Limit to a subject
+AND v.field_content LIKE '%Christmas%' AND (v.field_content LIKE '%decorations%' OR v.field_content LIKE '%history%' OR v.field_content LIKE '%cooking%' OR v.field_content LIKE '%Cook%')
+JOIN sierra_view.subfield s
+ON
+b.bib_record_id = s.record_id AND s.marc_tag = '020' AND s.tag = 'a'
+WHERE
+b.material_code = 'a' AND b.publish_year >= '2000'
+GROUP BY 1,2,3) a
+ORDER BY RANDOM()
+LIMIT 25)
+UNION
+(SELECT *
+FROM(
+SELECT
+--link to Encore
+DISTINCT 'https://find.minlib.net/iii/encore/record/C__R'||id2reckey(b.bib_record_id)   AS field_booklist_entry_encore_url,
+B.best_title as title,
+B.best_author as field_booklist_entry_author,
+'https://syndetics.com/index.aspx?isbn='||SUBSTRING(MAX(s.content) FROM '[0-9]+')||'/SC.gif&client=minuteman' AS field_booklist_entry_cover
+FROM
+sierra_view.bib_record_property b
+JOIN
+sierra_view.bib_record_item_record_link l
+ON
+b.bib_record_id = l.bib_record_id
+JOIN
+sierra_view.item_record i
+ON
+l.item_record_id = i.id
+AND
+i.is_available_at_library = 'TRUE'
+AND i.item_status_code NOT IN ('m', 'n', 'z', 't', 'o', '$', '!', 'w', 'd', 'p', 'r', 'e', 'j', 'u', 'q', 'x', 'y', 'v')
+AND i.itype_code_num NOT IN ('100', '101', '104', '105', '108', '150', '151', '152', '155', '156', '159', '160')
+AND SUBSTRING(i.location_code,4,1) NOT IN ('j','y')
+JOIN
+sierra_view.varfield_view v
+ON
+b.bib_record_id = v.record_id AND v.varfield_type_code = 'd' 
+--Limit to a subject
+AND v.field_content LIKE '%Hanukkah%'
+JOIN sierra_view.subfield s
+ON
+b.bib_record_id = s.record_id AND s.marc_tag = '020' AND s.tag = 'a'
+WHERE
+b.material_code = 'a' AND b.publish_year >= '1980'
+GROUP BY 1,2,3) a
+ORDER BY RANDOM()
+LIMIT 15)
+UNION
+(SELECT *
+FROM(
+SELECT
+--link to Encore
+DISTINCT 'https://find.minlib.net/iii/encore/record/C__R'||id2reckey(b.bib_record_id)   AS field_booklist_entry_encore_url,
+B.best_title as title,
+B.best_author as field_booklist_entry_author,
+'https://syndetics.com/index.aspx?isbn='||SUBSTRING(MAX(s.content) FROM '[0-9]+')||'/SC.gif&client=minuteman' AS field_booklist_entry_cover
+FROM
+sierra_view.bib_record_property b
+JOIN
+sierra_view.bib_record_item_record_link l
+ON
+b.bib_record_id = l.bib_record_id
+JOIN
+sierra_view.item_record i
+ON
+l.item_record_id = i.id
+--AND i.is_available_at_library = 'TRUE'
+AND i.item_status_code NOT IN ('m', 'n', 'z', 't', 'o', '$', '!', 'w', 'd', 'p', 'r', 'e', 'j', 'u', 'q', 'x', 'y', 'v')
+AND i.itype_code_num NOT IN ('100', '101', '104', '105', '108', '150', '151', '152', '155', '156', '159', '160')
+AND SUBSTRING(i.location_code,4,1) NOT IN ('j','y')
+JOIN
+sierra_view.varfield_view v
+ON
+b.bib_record_id = v.record_id AND v.varfield_type_code = 'd' 
+--Limit to a subject
+AND v.field_content LIKE '%Kwanza%' --AND (v.field_content LIKE '%decorations%' OR v.field_content LIKE '%history%' OR v.field_content LIKE '%cooking%' OR v.field_content LIKE '%Cook%')
+JOIN sierra_view.subfield s
+ON
+b.bib_record_id = s.record_id AND s.marc_tag = '020' AND s.tag = 'a'
+WHERE
+b.material_code = 'a' AND b.publish_year >= '1980'
+GROUP BY 1,2,3) a
+ORDER BY RANDOM()
+LIMIT 10)
---
+++ new overdrive ebook list.sql
@@ -0,0 +1,34 @@
+﻿/*
+Jeremy Goldstein
+Minuteman Library Network
+
+Returns 50 most recently added Ovedrive e-books for booklist at www.minlib.net
+*/
+
+SELECT
+--link to Encore
+'https://find.minlib.net/iii/encore/record/C__R'||id2reckey(b.bib_record_id)   AS field_booklist_entry_encore_url,
+b.best_title as title,
+b.best_author as field_booklist_entry_author,
+'https://syndetics.com/index.aspx?isbn='||SUBSTRING(MAX(s.content) FROM '[0-9]+')||'/SC.gif&client=minuteman' AS field_booklist_entry_cover
+FROM
+sierra_view.bib_record_property b
+--Grab ISBN for cover image
+JOIN sierra_view.subfield s
+ON
+b.bib_record_id = s.record_id AND s.marc_tag = '020' AND s.tag = 'a'
+--Limit to Overdrive records
+JOIN sierra_view.subfield s2
+ON
+b.bib_record_id = s2.record_id AND s2.marc_tag = '710' AND s.tag = 'a' AND s2.content = 'OverDrive, Inc.'
+JOIN sierra_view.bib_record_item_record_link bi
+ON
+b.bib_record_id = bi.bib_record_id
+JOIN sierra_view.record_metadata m
+ON
+bi.item_record_id = m.id AND m.creation_date_gmt > (localtimestamp - interval '7 days')
+--Limit to e-book mattype
+WHERE b.material_code ='h'
+GROUP BY 1,2,3 
+ORDER BY 1 Desc
+LIMIT 50;
