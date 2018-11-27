9de2df77a40aba5dbd83c38835763b7e83f77990
Jeremy Goldstein <JGoldstein@MLN.LOCAL>
2018-11-27 13:05:37 -0500

    top circ title by library

---
+++ staff picks_end of year.sql
@@ -0,0 +1,73 @@
+﻿--Lazy way to generate staff picks booklist
+SELECT
+--link to Encore
+'https://find.minlib.net/iii/encore/record/C__R'||id2reckey(b.bib_record_id)   AS field_booklist_entry_encore_url,
+b.best_title as title,
+b.best_author as field_booklist_entry_author,
+'https://syndetics.com/index.aspx?isbn='||SUBSTRING(MAX(s.content) FROM '[0-9]+')||'/SC.gif&client=minuteman' AS field_booklist_entry_cover
+FROM
+sierra_view.bib_record_property b
+JOIN sierra_view.subfield s
+ON
+b.bib_record_id = s.record_id AND s.marc_tag = '020' AND s.tag = 'a'
+JOIN sierra_view.bib_record_item_record_link bi
+ON
+b.bib_record_id = bi.bib_record_id
+JOIN
+sierra_view.bib_view v
+ON
+b.bib_record_id = v.id AND
+v.record_num IN (
+--an unkindness of ghosts
+'3661398',
+--moonshine
+'3613569',
+--vacationland
+'3652176',
+--how to invent everything
+'3769049',
+--Snagglepuss Chronicles
+'3783294',
+--Old Woman Laura
+'3770952',
+--Hawkeye kate bishop
+'3718917',
+--Spinning Silver
+'3755652',
+--Isola
+'3769849',
+--Batman White Knight
+'3768360',
+--Infidel
+'3766670',
+--Mae
+'3613565',
+--Thor
+'3755259',
+--Metal
+'3742087',
+--Rock Candy Mountain
+'3716159',
+--Lost Light
+'3754936',
+--Doctor Aphra
+'3673175',
+--Giant Days
+'3429620',
+--Head Lopper
+'3721269',
+--Wicked + Divine
+'3748062',
+--fall of the batmen
+'3784606',
+--Space Opera
+'3725134',
+--Freeze Frame Revolution
+'3748734',
+--Calculating Stars
+'3754471'
+)
+WHERE b.material_code ='a'
+GROUP BY 1,2,3 
+ORDER BY 2 Desc
+LIMIT 25;
---
+++ top circ new title by library.sql
@@ -0,0 +1,99 @@
+﻿/*
+Jeremy Goldstein
+Minuteman Library network
+
+Retrieves the top circulating title at each library based on items created within the past year
+*/
+
+DROP TABLE IF EXISTS temp_item_data
+;
+--gathers checkout data used by temp_loc_circ_counts table
+CREATE TEMP TABLE temp_item_data AS
+SELECT
+l.name AS library,
+i.id as item_id,
+i.checkout_total,
+i.renewal_total
+
+FROM
+sierra_view.item_view i
+
+--pull library name from location code
+JOIN
+sierra_view.location_myuser l
+on substring(i.location_code from 1 for 3) = l.code 
+
+--filter to items created in the past year
+WHERE
+i.record_creation_date_gmt > (localtimestamp - interval '1 year')
+
+;
+
+DROP TABLE IF EXISTS temp_loc_circ_counts;
+CREATE TEMP TABLE temp_loc_circ_counts AS
+SELECT
+t.library,
+b.bib_record_id,
+(SUM(t.checkout_total) + SUM(t.renewal_total)) as circ_total
+
+FROM
+temp_item_data as t
+
+JOIN
+sierra_view.bib_record_item_record_link l
+ON
+t.item_id = l.item_record_id
+JOIN sierra_view.bib_record_property b
+ON
+--limit to books
+l.bib_record_id = b.bib_record_id AND b.material_code = 'a'
+
+GROUP BY
+t.library,
+b.bib_record_id
+
+--filter out items that have not circed
+HAVING
+(SUM(t.checkout_total) + SUM(t.renewal_total)) > 1
+
+ORDER BY
+t.library,
+circ_total
+;
+
+SELECT
+--DISTINCT ON (library_name)
+t.library AS library_name,
+--Encore Link
+'https://find.minlib.net/iii/encore/record/C__R'||id2reckey(t.bib_record_id) AS field_booklist_entry_encore_url,
+b.best_title as title,
+b.best_author AS field_booklist_entry_author,
+--Cover image from Syndetics
+'https://syndetics.com/index.aspx?isbn='||SUBSTRING(MAX(s.content) FROM '[0-9]+')||'/SC.gif&client=minuteman' AS field_booklist_entry_cover,
+t.circ_total
+FROM
+(
+SELECT
+t2.library,
+MAX(t2.circ_total) as max_circ
+FROM
+temp_agency_circ_counts as t2
+GROUP BY 1
+) AS C
+
+JOIN
+temp_loc_circ_counts as t
+ON
+c.library = t.library AND c.max_circ = t.circ_total
+JOIN
+sierra_view.bib_record_property b
+ON
+t.bib_record_id = b.bib_record_id
+--Grab ISBN for cover image
+JOIN sierra_view.subfield s
+ON
+b.bib_record_id = s.record_id AND s.marc_tag = '020' AND s.tag = 'a'
+
+GROUP BY 1,2,3,4,6
+ORDER BY 1;
+
