ad1ed14b7ba62620c0444d5dcf1161026a4a262a
Jeremy Goldstein <JGoldstein@MLN.LOCAL>
2018-11-30 16:30:38 -0500

    staff picks and pop authors

--- popular authors.sql
+++ popular authors.sql
@@ -1,21 +1,65 @@
-﻿--top 100 authors by holds, 
---swap in commented lines to calculate by checkouts instead
+﻿--Jeremy Goldstein with Enormous assistance from Ray Voelker
+--Minuteman Library Network
+
+--Gathers the 100 most popular authors based on current holds
+
+
+DROP TABLE IF EXISTS temp_holds_data
+;
+--gathers hold data used by temp_location_holds_counts table
+CREATE TEMP TABLE temp_holds_data AS
 
 SELECT
-b.best_author,
-count (h.id)
---count (c.id)
-FROM
-sierra_view.hold h
---sierra_view.circ_trans c
-JOIN
-sierra_view.bib_record_property b
-ON
-h.record_id = b.bib_record_id
---c.bib_record_id = b.bib_record_id
-AND b.material_code = 'a'
---AND c.op_code = 'o'
-GROUP BY 1
-ORDER BY 2 DESC
+CASE
+    WHEN r.record_type_code = 'i' THEN (
+        SELECT
+        l.bib_record_id
+        FROM
+        sierra_view.bib_record_item_record_link as l
+        WHERE
+        l.item_record_id = h.record_id
+        LIMIT 1
+    )
+    WHEN r.record_type_code = 'j' THEN (
+        SELECT
+        l.bib_record_id
+        FROM
+        sierra_view.bib_record_volume_record_link as l
+        WHERE
+        l.volume_record_id = h.record_id
+        LIMIT 1
+    )
+    WHEN r.record_type_code = 'b' THEN (
+        h.record_id
+    )
+    ELSE NULL
+END AS bib_record_id
 
-LIMIT 101
+FROM
+sierra_view.hold as h
+JOIN
+sierra_view.record_metadata as r
+ON
+r.id = h.record_id
+;
+
+SELECT
+--still needs clean up
+array_to_string(regexp_matches(b.best_author, E'^(\\w+, \\w+).*$')) AS title,--AS field_booklist_entry_author,
+--MAX(b.best_title) AS title,
+'https://syndetics.com/index.aspx?isbn='||SUBSTRING((MAX(s.content) FILTER(WHERE b.material_code = 'a')) FROM '[0-9]+')||'/SC.gif&client=minuteman' AS field_booklist_entry_cover
+FROM
+sierra_view.bib_record_property b
+JOIN
+temp_holds_data t
+ON
+b.bib_record_id = t.bib_record_id
+--Pull ISBN for cover image
+JOIN sierra_view.subfield s
+ON
+b.bib_record_id = s.record_id AND s.marc_tag = '020' AND s.tag = 'a'
+WHERE
+b.best_author !=''
+GROUP BY 1
+ORDER BY COUNT(b.best_author) DESC
+LIMIT 100;
