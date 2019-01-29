68716a083455758ed61607fbf8ed1f28e15c5eee
Jeremy Goldstein <JGoldstein@MLN.LOCAL>
2019-01-29 15:05:51 -0500

    in transit / cam new

---
+++ cam new list.sql
@@ -0,0 +1,30 @@
+﻿/*
+Jeremy Goldstein
+Minuteman Library Network
+
+New Book list for www.minlib.net
+*/
+SELECT
+--link to Encore
+i.call_number_norm as call_number,
+'https://find.minlib.net/iii/encore/record/C__R'||id2reckey(b.bib_record_id)   AS url,
+b.best_title as title,
+b.best_author as author,
+ir.icode1 as scat
+FROM
+sierra_view.bib_record_property b
+JOIN sierra_view.bib_record_item_record_link bi
+ON
+b.bib_record_id = bi.bib_record_id
+JOIN sierra_view.item_record_property i
+ON
+bi.item_record_id = i.item_record_id
+JOIN sierra_view.item_record ir
+ON
+i.item_record_id = ir.id AND ir.location_code LIKE 'cam%'
+JOIN sierra_view.record_metadata m
+ON
+ir.id = m.id AND m.creation_date_gmt > (localtimestamp - interval '1 month')
+GROUP BY 2,3,4,1,5
+ORDER BY 1
+;
---
+++ transit snapshot.sql
@@ -0,0 +1,15 @@
+﻿-- In transit report
+--
+-- Based on query from Jason Boland
+--
+SELECT 
+  u.iii_user_group_code,  
+  l.name,
+  COUNT(v.field_content) AS "Count"  
+FROM sierra_view.varfield_view v
+JOIN sierra_view.item_view i ON v.record_id = i.id
+JOIN sierra_view.iii_user u ON SPLIT_PART(v.field_content, ' ', 9) = u.name
+JOIN sierra_view.location_myuser l ON SUBSTRING(SPLIT_PART(v.field_content, ' ', 11) FOR 3) = l.code
+WHERE field_content LIKE '%TRANSIT%' AND i.item_status_code = 't'
+GROUP BY 1,2
+ORDER BY 1,2;
