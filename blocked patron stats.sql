104dcad88ed14d47e0617b397341455d7af79af3
Jeremy Goldstein <JGoldstein@MLN.LOCAL>
2019-02-01 12:12:36 -0500

    fine experiments cont

---
+++ blocked patron stats.sql
@@ -0,0 +1,23 @@
+﻿/*Jeremy Goldstein
+  Minuteman Library Network
+  Collection of stats by library related to patron blocks and circulation activity
+*/
+
+SELECT
+l.name as home_library,
+COUNT(p.id) as total_patrons,
+COUNT(p.id) FILTER(WHERE ((p.mblock_code != '-') OR (p.owed_amt >= 10))) as total_block,
+cast(COUNT(p.id) FILTER(WHERE ((p.mblock_code != '-') OR (p.owed_amt >= 10))) as numeric (12,2)) / cast(COUNT(p.id) as numeric (12,2)) AS pct_blocked,
+DATE_TRUNC('day', AVG(AGE(now()::date,p.activity_gmt::date)) FILTER(WHERE ((p.mblock_code = '-') OR (p.owed_amt < 10)))) AS avg_last_active_not_blocked,
+DATE_TRUNC('day', AVG(AGE(now()::date,p.activity_gmt::date)) FILTER(WHERE ((p.mblock_code != '-') OR (p.owed_amt >= 10)))) AS avg_last_active_blocked,
+COUNT(p.id) FILTER(WHERE ((p.mblock_code = '-') OR (p.owed_amt < 10)) AND p.expiration_date_gmt < (now() + interval '1 year')) AS total_not_blocked_exp_this_year,
+COUNT(p.id) FILTER(WHERE ((p.mblock_code != '-') OR (p.owed_amt >= 10)) AND p.expiration_date_gmt < (now() + interval '1 year')) AS total_blocked_exp_this_year
+
+FROM
+sierra_view.patron_record p
+JOIN
+sierra_view.location_myuser l
+ON
+SUBSTRING(p.home_library_code FOR 3) = SUBSTRING(l.code FOR 3) AND l.code ~ '^[a-z1-9]{3}$'
+GROUP BY 1
+ORDER BY 1;
