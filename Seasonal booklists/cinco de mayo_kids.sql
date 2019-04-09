261ba5c45ef3bef4fc3f3c8351c7cb2e363bd0ec
Jeremy Goldstein <JGoldstein@MLN.LOCAL>
2019-04-09 13:36:47 -0400

    may lists

---
+++ Seasonal booklists/cinco de mayo_kids.sql
@@ -0,0 +1,51 @@
+﻿/*
+Jeremy Goldstein
+Minuteman Library Newtork
+
+Used to generate booklist at www.minlib.net
+*/
+SELECT *
+FROM(
+SELECT
+--link to Encore
+DISTINCT 'https://find.minlib.net/iii/encore/record/C__R'||id2reckey(b.bib_record_id)   AS field_booklist_entry_encore_url,
+b.best_title as title,
+REPLACE(SPLIT_PART(SPLIT_PART(b.best_author,' (',1),', ',2),'.','')||' '||SPLIT_PART(b.best_author,', ',1) as field_booklist_entry_author,
+--Generate cover image from Syndetics
+(SELECT
+'https://syndetics.com/index.aspx?isbn='||SUBSTRING(s.content FROM '[0-9]+')||'/SC.gif&client=minuteman'
+FROM
+sierra_view.subfield s
+WHERE
+b.bib_record_id = s.record_id AND s.marc_tag = '020' AND s.tag = 'a'
+ORDER BY s.occ_num
+LIMIT 1) AS field_booklist_entry_cover
+FROM
+sierra_view.bib_record_property b
+JOIN
+sierra_view.bib_record_item_record_link l
+ON
+b.bib_record_id = l.bib_record_id
+JOIN
+sierra_view.item_record i
+ON
+l.item_record_id = i.id
+--AND
+--i.is_available_at_library = 'TRUE'
+AND i.item_status_code NOT IN ('m', 'n', 'z', 't', 'o', '$', '!', 'w', 'd', 'p', 'r', 'e', 'j', 'u', 'q', 'x', 'y', 'v')
+--Limit to Jub collections
+AND SUBSTRING(i.location_code,4,1) IN ('j','y')
+--Limit to English
+JOIN
+sierra_view.bib_record r
+ON b.bib_record_id = r.id AND r.language_code = 'eng'
+JOIN
+sierra_view.phrase_entry d
+ON
+b.bib_record_id = d.record_id AND varfield_type_code = 'd'
+AND REPLACE(d.index_entry, ' ', '') LIKE '%cincodemayo%'
+WHERE
+b.material_code = 'a' AND b.publish_year >= '1990'
+GROUP BY 1,2,3,4) a
+ORDER BY RANDOM()
+LIMIT 50;
