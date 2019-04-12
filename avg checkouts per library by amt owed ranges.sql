a2138a25a7466d62ef79e6eca7fed678ce6158c1
Jeremy Goldstein <JGoldstein@MLN.LOCAL>
2019-04-12 10:55:27 -0400

    misc reports

---
+++ avg checkouts per library by amt owed ranges.sql
@@ -0,0 +1,12 @@
+﻿SELECT
+SUBSTRING(p.home_library_code FROM 1 FOR 3),
+ROUND(AVG(checkout_total + renewal_total) FILTER (WHERE owed_amt = '0'),2) AS "0_owed",
+ROUND(AVG(checkout_total + renewal_total) FILTER (WHERE owed_amt BETWEEN '0.1' AND '.99'),2) AS "<$1_owed",
+ROUND(AVG(checkout_total + renewal_total) FILTER (WHERE owed_amt BETWEEN '1' AND '2.49'),2) AS "<$2.50_owed",
+ROUND(AVG(checkout_total + renewal_total) FILTER (WHERE owed_amt BETWEEN '2.5' AND '4.99'),2) AS "<$5_owed",
+ROUND(AVG(checkout_total + renewal_total) FILTER (WHERE owed_amt BETWEEN '5' AND '9.99'),2) AS "<$10_owed",
+ROUND(AVG(checkout_total + renewal_total) FILTER (WHERE owed_amt >= '10'),2) AS blocked
+FROM
+sierra_view.patron_record p
+GROUP BY 1
+ORDER BY 1
---
+++ cam periodicals internal use report.sql
@@ -0,0 +1,39 @@
+﻿--Jeremy Goldstein
+--Minuteman Library Network
+
+--Gathers totals and subtotals (after running through Excel) of internal use counts for a week long project
+--Requested by Diana Fendler at CAM
+
+SELECT
+b.best_title,
+--REPLACE(ip.call_number,'|a',''),
+v.field_content AS volume,
+ip.barcode,
+--use count field being used tbd
+i.internal_use_count,
+copy_use_count,
+use3_count
+
+FROM
+sierra_view.circ_trans c
+JOIN
+sierra_view.bib_record_property b
+ON c.bib_record_id = b.bib_record_id
+JOIN
+sierra_view.item_record_property ip
+ON
+c.item_record_id = ip.item_record_id
+JOIN
+sierra_view.item_record i
+ON c.item_record_id = i.id
+LEFT JOIN
+sierra_view.varfield v
+ON c.item_record_id = v.record_id AND varfield_type_code = 'u'
+
+WHERE
+c.op_code = 'o' 
+AND SUBSTRING(c.item_location_code FROM 1 FOR 3) = 'cam'
+AND c.transaction_gmt BETWEEN '2019-04-10' AND '2019-04-12'
+
+ORDER BY 1,2,3,4
+;
