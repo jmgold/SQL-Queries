eff93be55fabe06d3cd17e919c9ba167d8d5ca4b
Jeremy Goldstein <JGoldstein@MLN.LOCAL>
2019-05-29 16:42:30 -0400

    rec2check

--- purchase alert with orders_concord_version.sql
+++ purchase alert with orders_concord_version.sql
@@ -1,6 +1,5 @@
 --Modified version of code shared by Gem Stone Logan over Sierra mailing list on 4/13/17
---Individual library coding needs to be updated on lines 11, 18, 34, 41, 43
---Modified to include holds by ptype column for Concord
+--Individual library coding needs to be updated on lines 12, 19, 35, 43, 45
 
 /* Drop the temp table if it already exists */
 DROP TABLE IF EXISTS mvhdholds;
@@ -9,7 +8,8 @@ DROP TABLE IF EXISTS mvhdholds;
 CREATE TEMP TABLE mvhdholds AS
 SELECT b.id AS "bib_id", 
 count(distinct h.id) as "hold_count",
-count (distinct CASE WHEN h.pickup_location_code LIKE 'co%' then h.id END) AS "local_holds",
+o1.order_locations as "order_locations",
+count (distinct CASE WHEN h.pickup_location_code IN ('co2z','conz') then h.id END) AS "local_holds",
 CASE
                 WHEN count(distinct i.id) IS NULL THEN 0
                 ELSE count(distinct i.id)
@@ -23,23 +23,24 @@ CASE
                 ELSE max(o1.order_copies)
         END
         AS "order_copies",
-count(distinct h.id) FILTER (WHERE p.ptype_code = '8') AS "total_holds_con_ptype"
+COUNT(DISTINCT h.id) FILTER (WHERE p.ptype_code = '8') AS "local_ptype_holds_count"
 FROM sierra_view.bib_record b
 LEFT JOIN sierra_view.bib_record_item_record_link bri
 on bri.bib_record_id=b.id
 LEFT JOIN sierra_view.hold h
 ON (h.record_id=b.id OR h.record_id=bri.item_record_id) AND h.status='0'
-JOIN sierra_view.patron_record p
+LEFT JOIN sierra_view.patron_record p
 ON
 h.patron_record_id = p.id
 LEFT JOIN sierra_view.item_record i
 ON i.id=bri.item_record_id
 LEFT JOIN sierra_view.item_record ia
-ON ia.id=bri.item_record_id AND ia.item_status_code not in
-('m','n','o','e','z','s','$','a','b','d','g','w') AND ((ia.location_code NOT LIKE 'co%' AND ia.itype_code_num not in ('5','21','109','133','160','183')) OR ia.location_code LIKE 'co%')
+ON ia.id=bri.item_record_id AND ia.item_status_code in
+('-','t','p','!') AND ((ia.location_code NOT LIKE 'co%' AND ia.itype_code_num not in ('5','21','109','133','160','183')) OR ia.location_code LIKE 'co%')
 LEFT JOIN (
         SELECT count(o.id) AS "order_count",
-        sum(oc.copies) AS "order_copies",
+        sum(oc.copies) AS "order_copies",        
+        string_agg(distinct(oc.location_code), ',') as "order_locations",
         bro.bib_record_id AS "bib_id"
         FROM sierra_view.bib_record_order_record_link BRO
         JOIN sierra_view.order_record o
@@ -48,7 +49,7 @@ LEFT JOIN (
         ON oc.order_record_id=o.id AND oc.location_code LIKE 'co%'
         WHERE o.order_status_code = 'o'
         GROUP BY bro.bib_record_id) o1 ON o1.bib_id=b.id
-GROUP BY b.id
+GROUP BY 1, 3
 HAVING
 count(distinct h.id)>0;
 
@@ -104,18 +105,19 @@ mv.hold_count as "TotalHoldCount",
 max(mv.local_avail_item_count) as "LocalAvailableItemCount",
 max(mv.order_copies) as "LocalOrderCopies",
 mv.local_holds as "LocalHoldCount",
-mv.total_holds_con_ptype AS TotalHoldsConPtype,
         CASE
 				WHEN max(mv.local_avail_item_count) + max(mv.order_copies)=0 THEN mv.local_holds
                 ELSE round(cast((mv.local_holds) as numeric (12, 2))/cast((max(mv.local_avail_item_count) + max(mv.order_copies)) as numeric(12,2)),2)
         END
         AS "LocalRatio",
-'http://find.minlib.net/iii/encore/record/C__R'||id2reckey(mv.bib_id)   AS "URL"
+'http://find.minlib.net/iii/encore/record/C__R'||id2reckey(mv.bib_id)   AS "URL",
+mv.order_locations AS "OrderLocations",
+mv.local_ptype_holds_count AS "LocalPtypeHoldsCount"
 from sierra_view.bib_record_property brp
 JOIN mvhdholds mv
 ON mv.bib_id=brp.bib_record_id
-GROUP BY 1, 2, 3, 4, 5, 6, 8, 12, 13, 15
+GROUP BY 1, 2, 3, 4, 5, 6, 8, 12, 14, 15, 16
 HAVING mv.local_holds > 0
 --(max(mv.item_count) + max(mv.order_copies))=0
 --OR max(mv.hold_count)/(max(mv.item_count) + max(mv.order_copies))>=4
-ORDER BY 5, 14 desc;
+ORDER BY 5, 13 desc;
