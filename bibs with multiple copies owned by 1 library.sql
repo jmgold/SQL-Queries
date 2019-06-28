a103d0a36561ef4c1bdb373fe61d0c8dd9ac3f5a
Jeremy Goldstein <JGoldstein@MLN.LOCAL>
2019-06-28 16:22:44 -0400

    updates

--- bibs with multiple copies owned by 1 library.sql
+++ bibs with multiple copies owned by 1 library.sql
@@ -1,34 +1,39 @@
 /*
 Jeremy Goldstein
 Minuteman Library Network
-
 use to gather bibs where a library owns multiple circulating copies of a title.
 And recent circ suggests that some copies could be weeded
 */
 
-select 'b'||bib.record_num||'a' as "record_num",
-bib.title,
+SELECT 
+id2reckey(b.bib_record_id)||'a' as "record_num",
+b.best_title,
 count(item.id) as "item_count",
 sum(item.last_year_to_date_checkout_total) as "last_ytd_checkouts",
-SUM(item.year_to_date_checkout_total) AS "ytd_checkouts"
+SUM(item.year_to_date_checkout_total) AS "ytd_checkouts",
+ROUND((SUM(item.year_to_date_checkout_total) + sum(item.last_year_to_date_checkout_total))::NUMERIC/count(item.id),2) AS turnover
 from 
-sierra_view.bib_view as bib
+sierra_view.bib_record_property as b
+JOIN
+sierra_view.bib_record bib
+ON
+b.bib_record_id = bib.id
+AND bib.bcode1 = 'm'
 join
 sierra_view.bib_record_item_record_link as link
 on
-bib.id = link.bib_record_id
+b.bib_record_id = link.bib_record_id
 join
 sierra_view.item_view as item
 on
-item.id = link.item_record_id AND item.item_status_code NOT IN ('o','w') AND item.agency_code_num = '38'
-where
---Limit to a location
-bib.bcode1 = 'm'
+item.id = link.item_record_id
+AND item.item_status_code NOT IN ('o','w') 
+AND item.location_code ~ '^ntn'
 
 
 
 group by 1, 2
-having count(case when item.agency_code_num = '38' then 1 else null end) > 1
-AND (SUM(item.year_to_date_checkout_total) + sum(item.last_year_to_date_checkout_total))/count(item.id) < 2
-order by 3 desc
+having count(item.id) > 1
+AND (SUM(item.year_to_date_checkout_total) + sum(item.last_year_to_date_checkout_total))::NUMERIC/count(item.id) < 2
+order BY 6,3 DESC
 ;
--- cam equipment circ.sql
+++ cam equipment circ.sql
@@ -1,6 +1,3 @@
-﻿--Jeremy Goldstein
---Minuteman Library network
-
 --Cambridge circ_trans data stripped of patron info for project run by Reinhard Engels
 
 SELECT
@@ -19,18 +16,23 @@ c.count_type_code_num,
 c.itype_code_num,
 c.icode1,
 c.item_location_code,
-c.loanrule_code_num
+c.loanrule_code_num,
+md5(CAST(c.patron_record_id AS VARCHAR)),
+p.pcode4
 
 FROM
 sierra_view.circ_trans c
 JOIN
 sierra_view.bib_record_property b
 ON
---Limited to equipment
 c.bib_record_id = b.bib_record_id and b.material_code IN ('q','r','v')
 JOIN
 sierra_view.item_record_property i
 ON
 c.item_record_id = i.item_record_id
+JOIN
+sierra_view.patron_record p
+ON
+c.patron_record_id = p.id
 WHERE
 item_agency_code_num = 3
