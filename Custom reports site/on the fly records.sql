a51f1106961be0b4e63e4b0f168c6cba2f3c3ef9
Jeremy Goldstein <JGoldstein@MLN.LOCAL>
2019-07-09 11:53:42 -0400

    custom reports site folder

---
+++ Custom reports site/on the fly records.sql
@@ -0,0 +1,45 @@
+/*Jeremy Goldstein
+Minuteman Library network
+
+Returns on the fly records more than 2 months old and not on an on the fly or ILL bib record
+Is passed variables for owning location, created before date, and whether to exclude items attached to an on the fly bib record
+*/
+
+select 
+id2reckey(i.id)||'a' as "item_number",
+b.title as "title",
+i.location_code as "location",
+REPLACE(ip.call_number,'|a','') AS call_number,
+v.field_content AS volume,
+i.barcode as "barcode",
+i.record_creation_date_gmt as "creation_date",
+i.last_checkin_gmt as "last_checkin"
+from
+sierra_view.item_view as i
+JOIN
+sierra_view.item_record_property ip
+ON
+i.id = ip.item_record_id
+JOIN
+sierra_view.bib_record_item_record_link           AS link
+ON
+i.id = link.item_record_id
+JOIN
+sierra_view.bib_view                             AS b
+ON
+link.bib_record_id = b.id 
+LEFT JOIN
+sierra_view.varfield v
+ON
+i.id = v.record_id AND v.varfield_type_code = 'v'
+where
+i.item_message_code = 'f'
+and
+i.record_creation_date_gmt::DATE < {{Created_Date}}
+{{#if Exclude}}
+and b.title not like '%fly'
+{{/if Exclude}}
+and b.title not like '%MLN ILL%'
+AND i.item_status_code != 'w'
+AND i.location_code ~ {{Location}}
+order by 3, 4;
---
+++ Custom reports site/orphaned order records.sql
@@ -0,0 +1,61 @@
+/*
+Jeremy Goldstein
+Minuteman Library Network
+
+Identifies bib records where a library has attached a paid order record and 0 items
+Limited to full monographic records
+
+Is passed an owning location variable
+*/
+
+SELECT
+DISTINCT id2reckey(b.bib_record_id)||'a' AS rec_num,
+b.best_title AS title,
+MAX(o.paid_date_gmt::DATE) AS paid_date
+FROM
+sierra_view.bib_record_property b
+JOIN
+sierra_view.bib_record br
+ON
+b.bib_record_id = br.id AND br.bcode1 = 'm' AND br.bcode3 NOT IN ('b','g','c')
+JOIN
+sierra_view.bib_record_order_record_link l
+ON
+b.bib_record_id = l.bib_record_id
+JOIN
+sierra_view.order_record_paid o
+ON
+l.order_record_id = o.order_record_id
+JOIN
+sierra_view.order_record_cmf cmf
+ON
+o.order_record_id = cmf.order_record_id AND cmf.location_code ~ {{Location}}
+JOIN
+sierra_view.order_record ord
+ON l.order_record_id = ord.id AND ord.order_status_code = 'a'
+WHERE NOT EXISTS (
+SELECT l.id
+FROM
+sierra_view.bib_record_item_record_link l
+JOIN
+sierra_view.item_record i 
+ON l.item_record_id = i.id
+AND b.bib_record_id = l.bib_record_id AND i.location_code ~ {{Location}}
+)
+AND EXISTS(
+SELECT l.id
+FROM
+sierra_view.bib_record_order_record_link l
+JOIN
+sierra_view.order_record o
+ON l.order_record_id = o.id
+AND
+b.bib_record_id = l.bib_record_id AND o.order_status_code = 'a'
+JOIN
+sierra_view.order_record_cmf cmf
+ON
+o.id = cmf.order_record_id AND cmf.location_code ~ {{Location}}
+
+)
+GROUP BY 1,2
+ORDER BY 3;
