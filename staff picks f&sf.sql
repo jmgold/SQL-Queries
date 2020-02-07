3069806387c058453de309719d5900e876b9b812
Jeremy Goldstein <JGoldstein@MLN.LOCAL>
2020-02-07 16:25:52 -0500

    staff picks

---
+++ staff picks f&sf.sql
@@ -0,0 +1,169 @@
+SELECT *
+FROM(
+SELECT
+--link to Encore, removed in favor of default keyword search on title
+--'https://find.minlib.net/iii/encore/record/C__R'||id2reckey(b.bib_record_id)   AS field_booklist_entry_encore_url,
+b.best_title as title,
+REPLACE(SPLIT_PART(SPLIT_PART(b.best_author,' (',1),', ',2),'.','')||' '||SPLIT_PART(b.best_author,', ',1) as field_booklist_entry_author,
+CASE
+WHEN b.material_code = 'a'
+THEN (SELECT
+'https://syndetics.com/index.aspx?isbn='||SUBSTRING(s.content FROM '[0-9]+')||'/SC.gif&client=minuteman'
+FROM
+sierra_view.subfield s
+WHERE
+b.bib_record_id = s.record_id AND s.marc_tag = '020' AND s.tag = 'a'
+ORDER BY s.occ_num
+LIMIT 1)
+WHEN b.material_code != 'a'
+THEN (SELECT
+'https://syndetics.com/index.aspx?upc='||SUBSTRING(s.content FROM '[0-9]+')||'/SC.gif&client=minuteman'
+FROM
+sierra_view.subfield s
+WHERE
+b.bib_record_id = s.record_id AND s.marc_tag = '024' AND s.tag = 'a'
+ORDER BY s.occ_num
+LIMIT 1)
+END AS field_booklist_entry_cover,
+v.record_num
+
+FROM
+sierra_view.bib_record_property b
+JOIN sierra_view.bib_record_item_record_link bi
+ON
+b.bib_record_id = bi.bib_record_id
+JOIN
+sierra_view.bib_view v
+ON
+b.bib_record_id = v.id AND
+v.record_num IN (
+--20200205 Jeremy Goldstein
+--The Deep
+'3825839',
+--Spinning Silver
+'3755652',
+--Space Opera
+'3725134',
+--Gideon the Ninth
+'3838330',
+--bayern agenda
+'3822208',
+--her body and other parties
+'3653799',
+--20200205 Dev Singer
+'3853104',
+--20200205 Sophie Forrester
+'2622125',
+'2903719',
+--20200205 Allison Smith
+'3792142',
+--20200205 Karen Donaghey
+'3675994',
+'1141593',
+--20200205 Mary Carter
+'3777938',
+'2968267',
+'3110294',
+'3163998',
+'3611335',
+'3272328',
+'2746659',
+'3107629',
+'3113502',
+'3621400',
+'3047845',
+'3223099',
+'3755652',
+'3095283',
+'2133780',
+'3010511',
+'2856180',
+'3020336',
+--20200205 Jillian Holmberg
+'3102202',
+'3718164',
+--20200205 Meghan Bouffard
+'2955063',
+'3539665',
+--20200205 Jennifer Keen
+'3010518',
+'2903719',
+'2105282',
+--20200205 Jess S
+'1581178',
+--20200205 Alicia Fernandez
+'3860008',
+'3735121',
+--20200205 Mariśca Mozeleski
+'3825771',
+'3829725',
+'3869137',
+'3838394',
+'3633283',
+'3800716',
+--20200205 Jessica Steytler
+'3526096',
+'3282745',
+'3237608',
+'3637136',
+'3800631',
+--20200205 Sam Sednek
+'3868364',
+--20200205 Quincy Knapp
+'3143812',
+'1909015',
+'3503527',
+'3755652',
+--20200205 Peter Hansen II
+'2409171',
+'3212431',
+'3079576',
+'3011713',
+--20200205 Jenny Arch
+'3853121',
+'3655837',
+'3651672',
+'2603916',
+'3170655',
+'3852145',
+'3656838',
+'3659252',
+'2927832',
+'3229184',
+'3195657',
+'3804175',
+'3788098',
+'3826374',
+'3802377',
+--20200206 Andrew Loof
+'3464408',
+'3141569',
+'2135225',
+'2713470',
+--20200206 Stefanie Claydon
+'1593162',
+'1996844',
+'2130806',
+'3530777',
+'3650498',
+'3808256',
+'3563894',
+'1979269',
+'3164123',
+'2902522',
+'3853402',
+'3838394',
+'3526096',
+'3654507',
+'3767055',
+'3223099',
+'3742476',
+'2610365',
+'2470540',
+'2003332'
+)
+GROUP BY b.bib_record_id,1,2,b.material_code,4 
+) a
+ORDER BY RANDOM()
+--LIMIT 50
+;
---
+++ staff picks teens.sql
@@ -0,0 +1,120 @@
+SELECT *
+FROM(
+SELECT
+--link to Encore, removed in favor of default keyword search on title
+--'https://find.minlib.net/iii/encore/record/C__R'||id2reckey(b.bib_record_id)   AS field_booklist_entry_encore_url,
+b.best_title as title,
+REPLACE(SPLIT_PART(SPLIT_PART(b.best_author,' (',1),', ',2),'.','')||' '||SPLIT_PART(b.best_author,', ',1) as field_booklist_entry_author,
+CASE
+WHEN b.material_code = 'a'
+THEN (SELECT
+'https://syndetics.com/index.aspx?isbn='||SUBSTRING(s.content FROM '[0-9]+')||'/SC.gif&client=minuteman'
+FROM
+sierra_view.subfield s
+WHERE
+b.bib_record_id = s.record_id AND s.marc_tag = '020' AND s.tag = 'a'
+ORDER BY s.occ_num
+LIMIT 1)
+WHEN b.material_code != 'a'
+THEN (SELECT
+'https://syndetics.com/index.aspx?upc='||SUBSTRING(s.content FROM '[0-9]+')||'/SC.gif&client=minuteman'
+FROM
+sierra_view.subfield s
+WHERE
+b.bib_record_id = s.record_id AND s.marc_tag = '024' AND s.tag = 'a'
+ORDER BY s.occ_num
+LIMIT 1)
+END AS field_booklist_entry_cover
+
+FROM
+sierra_view.bib_record_property b
+JOIN sierra_view.bib_record_item_record_link bi
+ON
+b.bib_record_id = bi.bib_record_id
+JOIN
+sierra_view.bib_view v
+ON
+b.bib_record_id = v.id AND
+v.record_num IN (
+--20200205 Jeremy Goldstein
+--Die
+'3814206',
+--good enough
+'3811315',
+--20200205 Dev Singer
+'3757176',
+--20200205 Jillian Holmberg
+'2213893',
+'3226168',
+'3102202',
+'3763603',
+--20200205 Meghan Bouffard
+'3815724',
+'2955063',
+'3539665',
+--20200205 Jennifer Keen
+'3010518',
+'3216412',
+'2105282',
+--20200205 Alicia Fernandez
+'3860008',
+'3734974',
+'3735121',
+--20200205 Mariśca Mozeleski
+'3825771',
+'3829725',
+'3864614',
+'3869137',
+'3858482',
+'3748410',
+'3800716',
+'3831661',
+--20200205 Sam Sednek
+'3868364',
+--20200205 Royce McGrath
+'3445745',
+'3804935',
+--20200205 Julie Kellndorfer
+'3825771',
+'3808188',
+'3659250',
+'3775527',
+'3817272',
+'3790582',
+'3853855',
+'3538318',
+'3661171',
+'3135210',
+'3142677',
+'3825862',
+--20200205 Peter Hansen II
+'2409171',
+'3212431',
+'2750217',
+'3079576',
+'3011713',
+--20200205 Rachel Simon
+'3859133',
+--20200205 Ariel Flowers
+'2199975',
+--20200206 Olivia Durant
+'3651672',
+--20200206 Ellen Jacobs
+'3171397',
+'3659356',
+'3763723',
+'3874244',
+'3524701',
+'2776793',
+'3023281',
+'2576963',
+'2394207',
+'2901477',
+'1969580',
+'1973173'
+)
+GROUP BY b.bib_record_id,1,2,b.material_code 
+) a
+ORDER BY RANDOM()
+LIMIT 50;
+;
