f855a192c4bccff30f74d0cbf5c7974633fb4f51
Jeremy Goldstein <JGoldstein@MLN.LOCAL>
2020-04-10 16:00:09 -0400

    Update course reserves - not in use.sql

--- Custom reports site/course reserves - not in use.sql
+++ Custom reports site/course reserves - not in use.sql
@@ -6,28 +6,35 @@ Is passed owning location as a variable
 
 */
 
-select 
-distinct id2reckey(bp.bib_record_id)||'a' AS bib_number,
-i.location_code as location,
-bp.best_title as title
-from
-sierra_view.item_record as i
-left join
-sierra_view.course_record_item_record_link as ci
+SELECT 
+DISTINCT id2reckey(bp.bib_record_id)||'a' AS bib_number,
+id2reckey(i.id)||'a' AS item_number,
+i.location_code AS location,
+bp.best_title AS title,
+TO_DATE(SUBSTRING(note.field_content,1,8), 'mm-dd-yy') AS off_reserve_date,
+SPLIT_PART(SPLIT_PART(note.field_content, ' CIRCED',1), 'FOR ', 2) AS course
+FROM
+sierra_view.item_record AS i
+LEFT JOIN
+sierra_view.course_record_item_record_link AS ci
 On 
 ci.item_record_id = i.id
 JOIN
-sierra_view.bib_record_item_record_link as bi
+sierra_view.bib_record_item_record_link AS bi
 ON
 bi.item_record_id = i.id
-Join sierra_view.bib_view as b
+Join sierra_view.bib_view AS b
 ON
 bi.bib_record_id = b.id
 JOIN sierra_view.bib_record_property bp
 ON
 b.id = bp.bib_record_id
-where
-ci.course_record_id is null and b.bcode3 = 'r'
+LEFT JOIN
+sierra_view.varfield note
+ON
+i.id = note.record_id AND note.varfield_type_code = 'r' AND note.field_content LIKE '%OFF RESERVE%'
+WHERE
+ci.course_record_id IS NULL AND b.bcode3 = 'r'
 AND i.location_code ~ {{Location}}
 
 order BY 2,3
