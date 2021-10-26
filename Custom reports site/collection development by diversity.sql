f175b30edba53e7c35cd26d40c1107181cb853f9
Jeremy Goldstein <jgoldstein@minlib.net>
2021-10-26 14:07:32 -0400

    dei updates

--- Custom reports site/collection development by diversity.sql
+++ Custom reports site/collection development by diversity.sql
@@ -1,3 +1,11 @@
+/*
+Jeremy Goldstein
+Minuteman Lirary Network
+Gathers together various performance metrics for portions of a library's collection
+grouped by diverse topical areas that were developed for the diversity analysis report found in this directory
+Is passed variables for owning location, item status to exclude from the report, age level and item created date
+*/
+
 WITH topic_list AS (SELECT *
 FROM
 (SELECT
@@ -15,7 +23,7 @@ CASE
 	WHEN REPLACE(d.index_entry,'.','') ~ '(south asia)|(indic)|(pakistan)|(\yindia\y)|(bengali)|(afghan(?!\swar))|(bangladesh)|(nepal)|(sri lanka)|(bhutan)' THEN 'South Asian'
 	WHEN REPLACE(d.index_entry,'.','') ~ '(east asia)|(asian americans)|(chin(a|ese))|(japan)|(korea)|(taiwan)|(vietnam)|(cambodia)|(mongolia)|(lao(s|tian))|(myanmar)|(malay)|(thai)|(philippin)|(indonesia)|(polynesia)|(brunei)|(east timor)|(pacific island)|(tibet autonomous)' THEN 'East Asian & Pacific Islander'
 	WHEN REPLACE(d.index_entry,'.','') ~ '(bullying)|(aggressiveness)|(abuse)|(violent crimes)|(violence)|(violence against)' THEN 'Abuse & Violence'
-	WHEN REPLACE(d.index_entry,'.','') ~ '(disabilit)|(blind)|(deaf)|(terminally ill)|(amputees)|(patients)|(aspergers)|(neurobehavioral)|(neuropsychology)|(neurodiversity)|(brain variation)|(personality disorder)|(autis(m|tic))' THEN 'Disabilities & Neurodiversity'
+	WHEN REPLACE(d.index_entry,'.','') ~ '((?<!recordings for people.*)disabilit)|(blind)|(deaf)|(terminally ill)|(amputees)|(patients)|(aspergers)|(neurobehavioral)|(neuropsychology)|(neurodiversity)|(brain variation)|(personality disorder)|(autis(m|tic))' THEN 'Disabilities & Neurodiversity'
    WHEN REPLACE(d.index_entry,'.','') ~ '(acceptance)|(anxiety)|(compulsive disorder)|(schizophrenia)|(eating disorders)|(mental (health)|(illness))|(resilience personality)|(suicid)|(self (esteem|confidence|acceptance))|(emotional problems)|(mindfulness)|(depressi)|(stress (psychology|disorder|psychology))' THEN 'Mental & Emotional Health'
 	WHEN REPLACE(d.index_entry,'.','') ~ '(gamblers)|(drug use)|(drug abuse)|(substance abuse)|(alcoholi)|(addiction)' THEN 'Substance Abuse & Addiction'
 	WHEN REPLACE(d.index_entry,'.','') ~ '(sexual minorities)|(gender)|(aesexual)|(bisexual)|(gay)|(intersex)|(homosexual)|(lesbian)|(stonewall riots)|(masculinity)|(femininity)' THEN 'LGBTQIA+ & Gender Studies'
@@ -41,8 +49,24 @@ JOIN
 sierra_view.phrase_entry d
 ON
 l.bib_record_id = d.record_id AND d.index_tag = 'd'
+JOIN
+sierra_view.record_metadata rmi
+ON
+i.id = rmi.id AND rmi.creation_date_gmt::DATE > {{created_date}}
+JOIN
+sierra_view.bib_record_property b
+ON
+l.bib_record_id = b.bib_record_id AND b.material_code IN ({{mat_type}}) 
 
-WHERE i.location_code ~ '^wsn'
+WHERE i.location_code ~ {{location}} AND i.item_status_code NOT IN ({{item_status_codes}})
+--location will take the form ^oln, which in this example looks for all locations starting with the string oln.
+AND {{age_limit}}
+/*
+(i.itype_code_num NOT BETWEEN '100' AND '183' AND SUBSTRING(i.location_code,4,1) NOT IN ('j','y')) -- adult
+i.itype_code_num BETWEEN '150' AND '183' OR SUBSTRING(i.location_code,4,1) = 'j')) --juv
+i.itype_code_num BETWEEN '100' AND '133' OR SUBSTRING(i.location_code,4,1) = 'y')) --ya
+i.location_code ~ '\w' --all ages
+*/
 GROUP BY 1,2,3,4,5,6
 )a
 WHERE
@@ -75,15 +99,17 @@ COUNT (DISTINCT i.id) FILTER(WHERE i.last_checkout_gmt IS NULL) AS "0_circs",
 ROUND(100.0 * (CAST(COUNT(DISTINCT i.id) FILTER(WHERE i.last_checkout_gmt IS NULL) AS NUMERIC (12,2)) / CAST(COUNT (DISTINCT i.id) AS NUMERIC (12,2))), 4)||'%' AS "Percentage_0_circs",
 ROUND((COUNT(DISTINCT i.id) *(AVG(i.price) FILTER(WHERE i.price>'0' AND i.price <'10000'))/(NULLIF((SUM(i.checkout_total) + SUM(i.renewal_total)),0))),2)::MONEY AS "Cost_Per_Circ_By_AVG_price",
 ROUND(CAST(SUM(i.checkout_total) + SUM(i.renewal_total) AS NUMERIC (12,2))/CAST(COUNT (i.id) AS NUMERIC (12,2)), 2) AS turnover,
-ROUND(100.0 * (CAST(COUNT(DISTINCT i.id) AS NUMERIC (12,2)) / (SELECT CAST(COUNT (DISTINCT i.id) AS NUMERIC (12,2))FROM sierra_view.item_record i WHERE i.location_code ~ '^wsn')), 6)||'%' AS relative_item_total,
-ROUND(100.0 * (CAST(SUM(i.checkout_total) + SUM(i.renewal_total) AS NUMERIC (12,2)) / (SELECT CAST(SUM(i.checkout_total) + SUM(i.renewal_total) AS NUMERIC (12,2)) FROM sierra_view.item_record i WHERE i.location_code ~ '^wsn')), 6)||'%' AS relative_circ
+ROUND(100.0 * (CAST(COUNT(DISTINCT i.id) AS NUMERIC (12,2)) / (SELECT CAST(COUNT (DISTINCT i.id) AS NUMERIC (12,2))FROM sierra_view.item_record i JOIN sierra_view.bib_record_item_record_link l ON i.id = l.item_record_id AND i.location_code ~ {{location}} AND i.item_status_code NOT IN ({{item_status_codes}}) AND {{age_limit}} JOIN
+	sierra_view.record_metadata rmi ON i.id = rmi.id AND rmi.creation_date_gmt::DATE > {{created_date}} JOIN sierra_view.bib_record_property b ON l.bib_record_id = b.bib_record_id AND b.material_code IN ({{mat_type}}) )), 6)||'%' AS relative_item_total,
+ROUND(100.0 * (CAST(SUM(i.checkout_total) + SUM(i.renewal_total) AS NUMERIC (12,2)) / (SELECT CAST(SUM(i.checkout_total) + SUM(i.renewal_total) AS NUMERIC (12,2)) FROM sierra_view.item_record i JOIN sierra_view.bib_record_item_record_link l ON i.id = l.item_record_id AND i.location_code ~ {{location}} AND i.item_status_code NOT IN ({{item_status_codes}}) AND {{age_limit}} JOIN
+	sierra_view.record_metadata rmi ON i.id = rmi.id AND rmi.creation_date_gmt::DATE > {{created_date}} JOIN sierra_view.bib_record_property b ON l.bib_record_id = b.bib_record_id AND b.material_code IN ({{mat_type}}) )), 6)||'%' AS relative_circ
 
 FROM
 sierra_view.item_record i
 JOIN
 sierra_view.bib_record_item_record_link l
 ON
-i.id = l.item_record_id AND i.location_code ~ '^wsn'
+i.id = l.item_record_id AND i.location_code ~ {{location}} AND i.item_status_code NOT IN ({{item_status_codes}}) AND {{age_limit}}
 LEFT JOIN
 topic_list t
 ON
@@ -92,6 +118,14 @@ LEFT JOIN
 sierra_view.checkout c
 ON
 i.id = c.item_record_id
+JOIN
+sierra_view.record_metadata rmi
+ON
+i.id = rmi.id AND rmi.creation_date_gmt::DATE > {{created_date}}
+JOIN
+sierra_view.bib_record_property b
+ON
+l.bib_record_id = b.bib_record_id AND b.material_code IN ({{mat_type}}) 
 
 GROUP BY 1
 
@@ -118,15 +152,17 @@ COUNT (DISTINCT i.id) FILTER(WHERE i.last_checkout_gmt IS NULL) AS "0_circs",
 ROUND(100.0 * (CAST(COUNT(DISTINCT i.id) FILTER(WHERE i.last_checkout_gmt IS NULL) AS NUMERIC (12,2)) / CAST(COUNT (DISTINCT i.id) AS NUMERIC (12,2))), 4)||'%' AS "Percentage_0_circs",
 ROUND((COUNT(DISTINCT i.id) *(AVG(i.price) FILTER(WHERE i.price>'0' AND i.price <'10000'))/(NULLIF((SUM(i.checkout_total) + SUM(i.renewal_total)),0))),2)::MONEY AS "Cost_Per_Circ_By_AVG_price",
 ROUND(CAST(SUM(i.checkout_total) + SUM(i.renewal_total) AS NUMERIC (12,2))/CAST(COUNT (i.id) AS NUMERIC (12,2)), 2) AS turnover,
-ROUND(100.0 * (CAST(COUNT(DISTINCT i.id) AS NUMERIC (12,2)) / (SELECT CAST(COUNT (DISTINCT i.id) AS NUMERIC (12,2))FROM sierra_view.item_record i WHERE i.location_code ~ '^wsn')), 6)||'%' AS relative_item_total,
-ROUND(100.0 * (CAST(SUM(i.checkout_total) + SUM(i.renewal_total) AS NUMERIC (12,2)) / (SELECT CAST(SUM(i.checkout_total) + SUM(i.renewal_total) AS NUMERIC (12,2)) FROM sierra_view.item_record i WHERE i.location_code ~ '^wsn')), 6)||'%' AS relative_circ
+ROUND(100.0 * (CAST(COUNT(DISTINCT i.id) AS NUMERIC (12,2)) / (SELECT CAST(COUNT (DISTINCT i.id) AS NUMERIC (12,2))FROM sierra_view.item_record i JOIN sierra_view.bib_record_item_record_link l ON i.id = l.item_record_id AND i.location_code ~ {{location}} AND i.item_status_code NOT IN ({{item_status_codes}}) AND {{age_limit}} JOIN
+	sierra_view.record_metadata rmi ON i.id = rmi.id AND rmi.creation_date_gmt::DATE > {{created_date}} JOIN sierra_view.bib_record_property b ON l.bib_record_id = b.bib_record_id AND b.material_code IN ({{mat_type}}) )), 6)||'%' AS relative_item_total,
+ROUND(100.0 * (CAST(SUM(i.checkout_total) + SUM(i.renewal_total) AS NUMERIC (12,2)) / (SELECT CAST(SUM(i.checkout_total) + SUM(i.renewal_total) AS NUMERIC (12,2)) FROM sierra_view.item_record i JOIN sierra_view.bib_record_item_record_link l ON i.id = l.item_record_id AND i.location_code ~ {{location}} AND i.item_status_code NOT IN ({{item_status_codes}}) AND {{age_limit}} JOIN
+	sierra_view.record_metadata rmi ON i.id = rmi.id AND rmi.creation_date_gmt::DATE > {{created_date}} JOIN sierra_view.bib_record_property b ON l.bib_record_id = b.bib_record_id AND b.material_code IN ({{mat_type}}) )), 6)||'%' AS relative_circ
 
 FROM
 sierra_view.item_record i
 JOIN
 sierra_view.bib_record_item_record_link l
 ON
-i.id = l.item_record_id AND i.location_code ~ '^wsn'
+i.id = l.item_record_id AND i.location_code ~ {{location}} AND i.item_status_code NOT IN ({{item_status_codes}}) AND {{age_limit}}
 JOIN
 (SELECT
 DISTINCT record_id
@@ -138,6 +174,14 @@ LEFT JOIN
 sierra_view.checkout c
 ON
 i.id = c.item_record_id
+JOIN
+sierra_view.record_metadata rmi
+ON
+i.id = rmi.id AND rmi.creation_date_gmt::DATE > {{created_date}}
+JOIN
+sierra_view.bib_record_property b
+ON
+l.bib_record_id = b.bib_record_id AND b.material_code IN ({{mat_type}}) 
 
 GROUP BY 1
 
