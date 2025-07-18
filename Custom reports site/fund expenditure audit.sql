6c9a70bd0150f8c9d7d2b4e2d6aebd659a333bca
Jeremy Goldstein <jgoldstein@minlib.net>
2025-07-18 14:49:36 -0400

    optimizing custom reports
    performance improvements to various queries used by our canned reports site.

--- Custom reports site/fund expenditure audit.sql
+++ Custom reports site/fund expenditure audit.sql
@@ -8,52 +8,45 @@ Compares current fund expenditures to the totals gathered from paid invoices
 --Gathers the expenditure break down for each fund within each invoice
 WITH invoice_total AS (
 SELECT
-i.id AS invoice_record_id,
+base.id AS invoice_record_id,
 fm.code AS fund_code,
 fm.id AS fm_id,
-SUM(l.paid_amt) + (SUM(COALESCE(l.copies_paid_cnt,1)) * ((i.shipping_amt + i.total_tax_amt + i.discount_amt) / COALESCE(copy_count.total_copies,1))) AS total,
-a.code_num
+SUM(base.paid_amt) + (SUM(COALESCE(base.copies_paid_cnt,1)) * ((base.shipping_amt + base.total_tax_amt + base.discount_amt) / COALESCE(base.total_copies,1))) AS total,
+a.code_num,
+a.id AS accounting_unit_id
 
-FROM
-sierra_view.invoice_record i
-JOIN
-sierra_view.invoice_record_line l
-ON
-i.id = l.invoice_record_id
+FROM (
+    SELECT 
+        i.*,
+        l.fund_code,
+        l.paid_amt,
+        l.copies_paid_cnt,
+        SUM(COALESCE(l.copies_paid_cnt,1)) OVER (PARTITION BY i.id) as total_copies
+    FROM
+        sierra_view.invoice_record i
+        JOIN sierra_view.invoice_record_line l ON i.id = l.invoice_record_id
+    WHERE
+        --i.paid_date_gmt or i.invoice_date_gmt
+        {{date_field}} >= {{date_limit}}::date
+        AND i.status_code = 'c'
+) base
 JOIN
 sierra_view.accounting_unit a
 ON
-i.accounting_unit_code_num = a.code_num AND a.code_num = {{accounting_unit}}
+base.accounting_unit_code_num = {{accounting_unit}} AND a.code_num = {{accounting_unit}}
 JOIN
 sierra_view.fund_master fm
 ON
-l.fund_code::INT = fm.code_num AND a.id = fm.accounting_unit_id
+base.fund_code::INT = fm.code_num AND a.id = fm.accounting_unit_id
 JOIN
 sierra_view.fund f
 ON
-fm.code = f.fund_code AND f.fund_type = 'fbal' AND a.code_num = f.acct_unit
---Pull total number of items for each invoice
-JOIN
-(SELECT
-l.invoice_record_id,
-SUM(l.copies_paid_cnt) AS total_copies
-FROM
-sierra_view.invoice_record_line l
-GROUP BY 1)AS copy_count
-ON
-i.id = copy_count.invoice_record_id
+fm.code = f.fund_code AND f.fund_type = 'fbal' AND f.acct_unit = {{accounting_unit}}
 
-WHERE
---i.paid_date_gmt or i.invoice_date_gmt
-{{date_field}}::DATE >= {{date_limit}}
-/*
-date_field options are
-i.invoice_date_gmt
-i.paid_date_gmt
-*/
-AND i.status_code = 'c'
 
-GROUP BY 1,2,3,5,i.shipping_amt,i.total_tax_amt,i.discount_amt,copy_count.total_copies)
+GROUP BY base.id, fm.code, fm.id, a.code_num, a.id,
+    base.shipping_amt, base.total_tax_amt, base.discount_amt, 
+    base.total_copies)
 
 SELECT
 *,
@@ -72,15 +65,11 @@ sierra_view.fund f
 JOIN
 invoice_total it
 ON
-f.fund_code = it.fund_code AND f.acct_unit = it.code_num AND f.fund_type = 'fbal'
-JOIN
-sierra_view.accounting_unit a
-ON
-f.acct_unit = a.code_num
+f.fund_code = it.fund_code AND f.acct_unit = {{accounting_unit}} AND f.fund_type = 'fbal'
 JOIN
 sierra_view.fund_master fm
 ON
-f.fund_code = fm.code AND a.id = fm.accounting_unit_id
+f.fund_code = fm.code AND it.accounting_unit_id = fm.accounting_unit_id
 JOIN
 sierra_view.fund_property fp
 ON
