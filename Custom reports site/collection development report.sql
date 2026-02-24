dec73880e1cf0705aadc19eb192ab410af9ea454
Jeremy Goldstein <jgoldstein@minlib.net>
2026-02-24 08:54:17 -0500

    misc query updates
    Mostly formatting improvements

--- Custom reports site/collection development report.sql
+++ Custom reports site/collection development report.sql
@@ -6,72 +6,61 @@ Is passed variables for owning location, item status to exclude from the report,
 */
 
 SELECT
-*,
-'' AS "COLLECTION DEVELOPMENT REPORT",
-'' AS "https://sic.minlib.net/reports/15"
-FROM
-(SELECT
-{{grouping}},
-/*Options are
-it.name AS itype
-ln.name AS language
-m.name AS mat_type
-i.icode1 AS scat_code
-i.location_code AS location
-*/
-COUNT (i.id) AS "Item total",
-SUM(i.checkout_total) AS "Total_Checkouts",
-SUM(i.renewal_total) AS "Total_Renewals",
-SUM(i.checkout_total) + SUM(i.renewal_total) AS "Total_Circulation",
-ROUND(AVG(i.price) FILTER(WHERE i.price>'0' AND i.price <'10000'),2)::MONEY AS "AVG_price",
-COUNT(i.id) FILTER(WHERE c.id IS NOT NULL) AS "total_checked_out",
-ROUND(100.0 * (CAST(COUNT(i.id) FILTER(WHERE c.id IS NOT NULL) AS NUMERIC (12,2)) / CAST(COUNT (i.id) AS NUMERIC (12,2))), 4)||'%' AS "Percentage_checked_out",
-COUNT (i.id) FILTER(WHERE i.last_checkout_gmt >= (localtimestamp - interval '1 year')) AS "have_circed_within_1_year",
-ROUND(100.0 * (CAST(COUNT(i.id) FILTER(WHERE i.last_checkout_gmt >= (localtimestamp - interval '1 year')) AS NUMERIC (12,2)) / CAST(COUNT (i.id) AS NUMERIC (12,2))), 4)||'%' AS "Percentage_1_year",
-COUNT (i.id) FILTER(WHERE i.last_checkout_gmt >= (localtimestamp - interval '3 years')) AS "have_circed_within_3_years",
-ROUND(100.0 * (CAST(COUNT(i.id) FILTER(WHERE i.last_checkout_gmt >= (localtimestamp - interval '3 years')) AS NUMERIC (12,2)) / CAST(COUNT (i.id) AS NUMERIC (12,2))), 4)||'%' AS "Percentage_3_years",
-COUNT (i.id) FILTER(WHERE i.last_checkout_gmt >= (localtimestamp - interval '5 years')) AS "have_circed_within_5_years",
-ROUND(100.0 * (CAST(COUNT(i.id) FILTER(WHERE i.last_checkout_gmt >= (localtimestamp - interval '5 years')) AS NUMERIC (12,2)) / CAST(COUNT (i.id) AS NUMERIC (12,2))), 4)||'%' AS "Percentage_5_years",
-COUNT (i.id) FILTER(WHERE i.last_checkout_gmt is not null) AS "have_circed_within_5+_years",
-ROUND(100.0 * (CAST(COUNT(i.id) FILTER(WHERE i.last_checkout_gmt IS NOT NULL) AS NUMERIC (12,2)) / CAST(COUNT (i.id) AS NUMERIC (12,2))), 4)||'%' AS "Percentage_5+_years",
-COUNT (i.id) FILTER(WHERE i.last_checkout_gmt is null) AS "0_circs",
-ROUND(100.0 * (CAST(COUNT(i.id) FILTER(WHERE i.last_checkout_gmt IS NULL) AS NUMERIC (12,2)) / CAST(COUNT (i.id) AS NUMERIC (12,2))), 4)||'%' AS "Percentage_0_circs",
-ROUND((COUNT(i.id) *(AVG(i.price) FILTER(WHERE i.price>'0' AND i.price <'10000'))/(NULLIF((SUM(i.checkout_total) + SUM(i.renewal_total)),0))),2)::MONEY AS "Cost_Per_Circ_By_AVG_price",
-ROUND(CAST(SUM(i.checkout_total) + SUM(i.renewal_total) AS numeric (12,2))/CAST(COUNT (i.id) as NUMERIC (12,2)), 2) AS turnover,
-ROUND(100.0 * (CAST(COUNT(i.id) AS numeric (12,2)) / (SELECT CAST(COUNT (i.id) AS NUMERIC (12,2))FROM sierra_view.item_record i WHERE i.location_code ~ '{{location}}' AND i.item_status_code NOT IN ({{Item_Status_Codes}}))), 6)||'%' AS relative_item_total,
-ROUND(100.0 * (CAST(SUM(i.checkout_total) + SUM(i.renewal_total) AS NUMERIC (12,2)) / (SELECT CAST(SUM(i.checkout_total) + SUM(i.renewal_total) AS NUMERIC (12,2)) FROM sierra_view.item_record i WHERE i.location_code ~ '{{location}}' AND i.item_status_code NOT IN ({{Item_Status_Codes}}))), 6)||'%' AS relative_circ
-FROM
-sierra_view.item_record i
-JOIN
-sierra_view.item_record_property ip
-ON
-i.id = ip.item_record_id
-JOIN
-sierra_view.bib_record_item_record_link l
-ON
-i.id = l.item_record_id
-JOIN sierra_view.bib_record b
-ON
-l.bib_record_id = b.id
-LEFT JOIN
-sierra_view.checkout c
-ON
-i.id = c.item_record_id
-JOIN
-sierra_view.material_property_myuser m
-ON
-b.bcode2 = m.code
-JOIN
-sierra_view.itype_property_myuser it
-ON
-i.itype_code_num = it.code
-LEFT JOIN
-sierra_view.language_property_myuser LN
-ON
-b.language_code = ln.code
-WHERE location_code ~ '{{location}}'
---location will take the form ^oln, which in this example looks for all locations starting with the string oln.
-AND item_status_code NOT IN ({{Item_Status_Codes}})
-GROUP BY 1
-ORDER BY 1
+  *,
+  '' AS "COLLECTION DEVELOPMENT REPORT",
+  '' AS "https://sic.minlib.net/reports/15"
+
+FROM (
+  SELECT
+    {{grouping}},
+    /*Options are
+    it.name AS itype
+    ln.name AS language
+    m.name AS mat_type
+    i.icode1 AS scat_code
+    i.location_code AS location
+    */
+    COUNT (i.id) AS "Item total",
+    SUM(i.checkout_total) AS "Total_Checkouts",
+    SUM(i.renewal_total) AS "Total_Renewals",
+    SUM(i.checkout_total) + SUM(i.renewal_total) AS "Total_Circulation",
+    ROUND(AVG(i.price) FILTER(WHERE i.price>'0' AND i.price <'10000'),2)::MONEY AS "AVG_price",
+    COUNT(i.id) FILTER(WHERE c.id IS NOT NULL) AS "total_checked_out",
+    ROUND(100.0 * (CAST(COUNT(i.id) FILTER(WHERE c.id IS NOT NULL) AS NUMERIC (12,2)) / CAST(COUNT (i.id) AS NUMERIC (12,2))), 4)||'%' AS "Percentage_checked_out",
+    COUNT (i.id) FILTER(WHERE i.last_checkout_gmt >= (localtimestamp - interval '1 year')) AS "have_circed_within_1_year",
+    ROUND(100.0 * (CAST(COUNT(i.id) FILTER(WHERE i.last_checkout_gmt >= (localtimestamp - interval '1 year')) AS NUMERIC (12,2)) / CAST(COUNT (i.id) AS NUMERIC (12,2))), 4)||'%' AS "Percentage_1_year",
+    COUNT (i.id) FILTER(WHERE i.last_checkout_gmt >= (localtimestamp - interval '3 years')) AS "have_circed_within_3_years",
+    ROUND(100.0 * (CAST(COUNT(i.id) FILTER(WHERE i.last_checkout_gmt >= (localtimestamp - interval '3 years')) AS NUMERIC (12,2)) / CAST(COUNT (i.id) AS NUMERIC (12,2))), 4)||'%' AS "Percentage_3_years",
+    COUNT (i.id) FILTER(WHERE i.last_checkout_gmt >= (localtimestamp - interval '5 years')) AS "have_circed_within_5_years",
+    ROUND(100.0 * (CAST(COUNT(i.id) FILTER(WHERE i.last_checkout_gmt >= (localtimestamp - interval '5 years')) AS NUMERIC (12,2)) / CAST(COUNT (i.id) AS NUMERIC (12,2))), 4)||'%' AS "Percentage_5_years",
+    COUNT (i.id) FILTER(WHERE i.last_checkout_gmt is not null) AS "have_circed_within_5+_years",
+    ROUND(100.0 * (CAST(COUNT(i.id) FILTER(WHERE i.last_checkout_gmt IS NOT NULL) AS NUMERIC (12,2)) / CAST(COUNT (i.id) AS NUMERIC (12,2))), 4)||'%' AS "Percentage_5+_years",
+    COUNT (i.id) FILTER(WHERE i.last_checkout_gmt is null) AS "0_circs",
+    ROUND(100.0 * (CAST(COUNT(i.id) FILTER(WHERE i.last_checkout_gmt IS NULL) AS NUMERIC (12,2)) / CAST(COUNT (i.id) AS NUMERIC (12,2))), 4)||'%' AS "Percentage_0_circs",
+    ROUND((COUNT(i.id) *(AVG(i.price) FILTER(WHERE i.price>'0' AND i.price <'10000'))/(NULLIF((SUM(i.checkout_total) + SUM(i.renewal_total)),0))),2)::MONEY AS "Cost_Per_Circ_By_AVG_price",
+    ROUND(CAST(SUM(i.checkout_total) + SUM(i.renewal_total) AS numeric (12,2))/CAST(COUNT (i.id) as NUMERIC (12,2)), 2) AS turnover,
+    ROUND(100.0 * (CAST(COUNT(i.id) AS numeric (12,2)) / (SELECT CAST(COUNT (i.id) AS NUMERIC (12,2))FROM sierra_view.item_record i WHERE i.location_code ~ '{{location}}' AND i.item_status_code NOT IN ({{Item_Status_Codes}}))), 6)||'%' AS relative_item_total,
+    ROUND(100.0 * (CAST(SUM(i.checkout_total) + SUM(i.renewal_total) AS NUMERIC (12,2)) / (SELECT CAST(SUM(i.checkout_total) + SUM(i.renewal_total) AS NUMERIC (12,2)) FROM sierra_view.item_record i WHERE i.location_code ~ '{{location}}' AND i.item_status_code NOT IN ({{Item_Status_Codes}}))), 6)||'%' AS relative_circ
+
+  FROM sierra_view.item_record i
+  JOIN sierra_view.item_record_property ip
+    ON i.id = ip.item_record_id
+  JOIN sierra_view.bib_record_item_record_link l
+    ON i.id = l.item_record_id
+  JOIN sierra_view.bib_record b
+    ON l.bib_record_id = b.id
+  LEFT JOIN sierra_view.checkout c
+    ON i.id = c.item_record_id
+  JOIN sierra_view.material_property_myuser m
+    ON b.bcode2 = m.code
+  JOIN sierra_view.itype_property_myuser it
+    ON i.itype_code_num = it.code
+  LEFT JOIN sierra_view.language_property_myuser ln
+    ON b.language_code = ln.code
+
+  WHERE i.location_code ~ '{{location}}'
+  --location will take the form ^oln, which in this example looks for all locations starting with the string oln.
+    AND i.item_status_code NOT IN ({{Item_Status_Codes}})
+  GROUP BY 1
+  ORDER BY 1
 )a
--- Custom reports site/serials - last issue received.sql
+++ Custom reports site/serials - last issue received.sql
@@ -7,99 +7,81 @@ Passed owning location as a variable
 */
 
 SELECT
-t1.rec_num AS bib_number,
-t1.title,
-t1.enum_level_a AS volume,
-t1.enum_level_b AS "number",
-t1.cover_date,
-'' AS "SERIALS: LAST ISSUE RECEIVED",
-'' AS "https://sic.minlib.net/reports/23"
+  t1.rec_num AS bib_number,
+  t1.title,
+  t1.enum_level_a AS volume,
+  t1.enum_level_b AS "number",
+  t1.cover_date,
+  '' AS "SERIALS: LAST ISSUE RECEIVED",
+  '' AS "https://sic.minlib.net/reports/23"
 
-FROM 
-(SELECT
-id2reckey(b.bib_record_id)||'a' AS rec_num,
-b.best_title as title,
-MAX(hb.id) as max_id,
-hb.enum_level_a,
-hb.enum_level_b,
-CASE
-	WHEN hb.chron_level_i ~ '^\d{4}$' AND hb.chron_level_j ~ '^\d{2}$'AND hb.chron_level_k ~ '^\d{2}$' THEN hb.chron_level_j||'-'||hb.chron_level_k||'-'||hb.chron_level_i
-	WHEN hb.chron_level_i ~ '^\d{4}$' AND hb.chron_level_j ~ '^\d{2}$'AND hb.chron_level_k IS NULL THEN hb.chron_level_j||'-'||hb.chron_level_i
-	WHEN (hb.chron_level_k IS NULL OR hb.chron_level_k = '0-') AND hb.chron_level_i ~ '^\d{4}$' AND hb.chron_level_j ~ '^\d{2}(\/|\-)\d{2}$'
-		THEN LEFT(hb.chron_level_j,2)||'-'||hb.chron_level_i||' / '||RIGHT(hb.chron_level_j,2)||'-'||hb.chron_level_i
-	WHEN (hb.chron_level_k IS NULL OR hb.chron_level_k = '0-') AND hb.chron_level_i ~ '^\d{4}(\/|\-)\d{4}$' AND  hb.chron_level_j ~ '^\d{2}(\/|\-)\d{2}$'
-		THEN LEFT(hb.chron_level_j,2)||'-'||LEFT(hb.chron_level_i,4)||' / '||RIGHT(hb.chron_level_j,2)||'-'||RIGHT(hb.chron_level_i,4)
-	WHEN hb.chron_level_i ~ '^\d{4}$' AND hb.chron_level_j ~ '^\d{2}(\/|\-)\d{2}$' AND hb.chron_level_k ~ '^\d{1,2}(\/|\-)\d{1,2}$'
-		THEN LEFT(hb.chron_level_j,2)||'-'||SUBSTRING(hb.chron_level_k,'^[0-9]{1,2}')||'-'||hb.chron_level_i||' / '||RIGHT(hb.chron_level_j,2)||'-'||SUBSTRING(hb.chron_level_k,'\d{1,2}$')||'-'||hb.chron_level_i
-	WHEN hb.chron_level_i ~ '^\d{4}$' AND hb.chron_level_j ~ '^\d{2}$' AND hb.chron_level_k ~ '^\d{1,2}(\/|\-)\d{1,2}$'
-		THEN hb.chron_level_j||'-'||SUBSTRING(hb.chron_level_k,'^\d{1,2}')||'-'||hb.chron_level_i||' / '||hb.chron_level_j||'-'||SUBSTRING(hb.chron_level_k,'\d{1,2}$')||'-'||hb.chron_level_i
-	WHEN hb.chron_level_i ~ '^\d{4}(\/|\-)\d{4}$' AND hb.chron_level_j ~ '^\d{2}(\/|\-)\d{2}$' AND hb.chron_level_k ~ '^\d{1,2}(\/|\-)\d{1,2}$'
-		THEN LEFT(hb.chron_level_j,2)||'-'||SUBSTRING(hb.chron_level_k,'^\d{1,2}')||'-'||LEFT(hb.chron_level_i,4)||' / '||RIGHT(hb.chron_level_j,2)||'-'||SUBSTRING(hb.chron_level_k,'\d{1,2}$')||'-'||RIGHT(hb.chron_level_i,4)
-	ELSE hb.chron_level_i||' '||hb.chron_level_j||' '||hb.chron_level_k
-END AS cover_date
-FROM
-sierra_view.bib_record_property b
-JOIN
-sierra_view.bib_record_holding_record_link l
-ON
-b.bib_record_id = l.bib_record_id
-JOIN
-sierra_view.holding_record h
-ON
-l.holding_record_id = h.id
-JOIN
-sierra_view.holding_record_location hl
-ON
-h.id = hl.holding_record_id AND hl.location_code ~ '{{location}}'
---location will take the form ^oln, which in this example looks for all locations starting with the string oln.
-JOIN
-sierra_view.holding_record_card hc
-ON
-h.id = hc.holding_record_id
-JOIN
-sierra_view.holding_record_cardlink cl
-ON
-hc.id = cl.holding_record_card_id
-JOIN
-sierra_view.holding_record_box hb
-ON
-cl.id = hb.holding_record_cardlink_id AND box_status_code = 'A'
-GROUP BY 1,2,4,5,6
-ORDER BY 2) t1
+FROM (
+  SELECT
+    id2reckey(b.bib_record_id)||'a' AS rec_num,
+    b.best_title as title,
+    MAX(hb.id) as max_id,
+    hb.enum_level_a,
+    hb.enum_level_b,
+    CASE
+	   WHEN hb.chron_level_i ~ '^\d{4}$' AND hb.chron_level_j ~ '^\d{2}$'AND hb.chron_level_k ~ '^\d{2}$' THEN hb.chron_level_j||'-'||hb.chron_level_k||'-'||hb.chron_level_i
+	   WHEN hb.chron_level_i ~ '^\d{4}$' AND hb.chron_level_j ~ '^\d{2}$'AND hb.chron_level_k IS NULL THEN hb.chron_level_j||'-'||hb.chron_level_i
+	   WHEN (hb.chron_level_k IS NULL OR hb.chron_level_k = '0-') AND hb.chron_level_i ~ '^\d{4}$' AND hb.chron_level_j ~ '^\d{2}(\/|\-)\d{2}$'
+		  THEN LEFT(hb.chron_level_j,2)||'-'||hb.chron_level_i||' / '||RIGHT(hb.chron_level_j,2)||'-'||hb.chron_level_i
+	   WHEN (hb.chron_level_k IS NULL OR hb.chron_level_k = '0-') AND hb.chron_level_i ~ '^\d{4}(\/|\-)\d{4}$' AND  hb.chron_level_j ~ '^\d{2}(\/|\-)\d{2}$'
+		  THEN LEFT(hb.chron_level_j,2)||'-'||LEFT(hb.chron_level_i,4)||' / '||RIGHT(hb.chron_level_j,2)||'-'||RIGHT(hb.chron_level_i,4)
+	   WHEN hb.chron_level_i ~ '^\d{4}$' AND hb.chron_level_j ~ '^\d{2}(\/|\-)\d{2}$' AND hb.chron_level_k ~ '^\d{1,2}(\/|\-)\d{1,2}$'
+		  THEN LEFT(hb.chron_level_j,2)||'-'||SUBSTRING(hb.chron_level_k,'^[0-9]{1,2}')||'-'||hb.chron_level_i||' / '||RIGHT(hb.chron_level_j,2)||'-'||SUBSTRING(hb.chron_level_k,'\d{1,2}$')||'-'||hb.chron_level_i
+	   WHEN hb.chron_level_i ~ '^\d{4}$' AND hb.chron_level_j ~ '^\d{2}$' AND hb.chron_level_k ~ '^\d{1,2}(\/|\-)\d{1,2}$'
+		  THEN hb.chron_level_j||'-'||SUBSTRING(hb.chron_level_k,'^\d{1,2}')||'-'||hb.chron_level_i||' / '||hb.chron_level_j||'-'||SUBSTRING(hb.chron_level_k,'\d{1,2}$')||'-'||hb.chron_level_i
+	   WHEN hb.chron_level_i ~ '^\d{4}(\/|\-)\d{4}$' AND hb.chron_level_j ~ '^\d{2}(\/|\-)\d{2}$' AND hb.chron_level_k ~ '^\d{1,2}(\/|\-)\d{1,2}$'
+		  THEN LEFT(hb.chron_level_j,2)||'-'||SUBSTRING(hb.chron_level_k,'^\d{1,2}')||'-'||LEFT(hb.chron_level_i,4)||' / '||RIGHT(hb.chron_level_j,2)||'-'||SUBSTRING(hb.chron_level_k,'\d{1,2}$')||'-'||RIGHT(hb.chron_level_i,4)
+	   ELSE hb.chron_level_i||' '||hb.chron_level_j||' '||hb.chron_level_k
+    END AS cover_date
 
-JOIN 
-(SELECT 
-b.best_title, 
-MAX(hb.id) AS max_id 
-FROM 
-sierra_view.bib_record_property b
-JOIN
-sierra_view.bib_record_holding_record_link l
-ON
-b.bib_record_id = l.bib_record_id
-JOIN
-sierra_view.holding_record h
-ON
-l.holding_record_id = h.id 
-JOIN
-sierra_view.holding_record_location hl
-ON
-h.id = hl.holding_record_id AND hl.location_code ~ {{location}}
---location will take the form ^oln, which in this example looks for all locations starting with the string oln.
-JOIN
-sierra_view.holding_record_card hc
-ON
-h.id = hc.holding_record_id
-JOIN
-sierra_view.holding_record_cardlink cl
-ON
-hc.id = cl.holding_record_card_id
-JOIN
-sierra_view.holding_record_box hb
-ON
-cl.id = hb.holding_record_cardlink_id AND box_status_code = 'A'
-GROUP BY 1) t2
+  FROM sierra_view.bib_record_property b
+  JOIN sierra_view.bib_record_holding_record_link l
+    ON b.bib_record_id = l.bib_record_id
+  JOIN sierra_view.holding_record h
+    ON l.holding_record_id = h.id
+  JOIN sierra_view.holding_record_location hl
+    ON h.id = hl.holding_record_id
+  JOIN sierra_view.holding_record_card hc
+    ON h.id = hc.holding_record_id
+  JOIN sierra_view.holding_record_cardlink cl
+    ON hc.id = cl.holding_record_card_id
+  JOIN sierra_view.holding_record_box hb
+    ON cl.id = hb.holding_record_cardlink_id
+	 AND box_status_code = 'A'
+  
+  --location will take the form ^oln, which in this example looks for all locations starting with the string oln.
+  WHERE hl.location_code ~ '{{location}}'
+  GROUP BY 1,2,4,5,6
+  ORDER BY 2
+) t1
 
-ON
-t1.max_id = t2.max_id
+JOIN (
+  SELECT 
+    b.best_title, 
+    MAX(hb.id) AS max_id 
+
+  FROM sierra_view.bib_record_property b
+  JOIN sierra_view.bib_record_holding_record_link l
+    ON b.bib_record_id = l.bib_record_id
+  JOIN sierra_view.holding_record h
+    ON l.holding_record_id = h.id 
+  JOIN sierra_view.holding_record_location hl
+    ON h.id = hl.holding_record_id
+  JOIN sierra_view.holding_record_card hc
+    ON h.id = hc.holding_record_id
+  JOIN sierra_view.holding_record_cardlink cl
+    ON hc.id = cl.holding_record_card_id
+  JOIN sierra_view.holding_record_box hb
+    ON cl.id = hb.holding_record_cardlink_id
+	 AND box_status_code = 'A'
+
+  WHERE hl.location_code ~ '{{location}}'
+  --location will take the form ^oln, which in this example looks for all locations starting with the string oln.
+  GROUP BY 1
+) t2
+  ON t1.max_id = t2.max_id
 ORDER BY 2
